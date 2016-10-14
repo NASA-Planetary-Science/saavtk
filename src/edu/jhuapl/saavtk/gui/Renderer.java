7b296ab879a60a6f8f95cce75ecc1e289ef73d09
Mike Zimmerman <Michael.Zimmerman@jhuapl.edu>
2016-10-14 11:28:29 -0400

    Applied animated interpolation between camera states in setCameraLatLon and setCameraFocalPoint methods in the Renderer class, which was added to the setCameraOrientation method in a previous commit. The CameraState class (nested in the Renderer class) might be at least partially redundant with the CameraFrame class (also nested in Renderer).

--- src/edu/jhuapl/saavtk/gui/Renderer.java
+++ src/edu/jhuapl/saavtk/gui/Renderer.java
@@ -442,6 +442,14 @@ public class Renderer extends JPanel implements
     	Vector3D up;
     	double viewAngle;
     	
+    	public CameraState(double[] pos, double[] focPt, double[] up, double viewAngle)
+    	{
+    		this.position=new Vector3D(pos);
+    		this.focalPoint=new Vector3D(focPt);
+    		this.up=new Vector3D(up);
+    		this.viewAngle=viewAngle;
+    	}
+    	
     	public CameraState(Vector3D pos, Vector3D focPt, Vector3D up, double viewAngle)
     	{
 			this.position=pos;
@@ -1135,26 +1143,19 @@ public class Renderer extends JPanel implements
         pos[0] *= distance;
         pos[1] *= distance;
         pos[2] *= distance;
-
-        // Set the new camera position
-        mainCanvas.getVTKLock().lock();
-        cam.SetPosition(pos);
-        mainCanvas.getVTKLock().unlock();
-        mainCanvas.resetCameraClippingRange();
-        mainCanvas.Render();
+        
+        CameraState finalState=new CameraState(pos, cam.GetFocalPoint(), cam.GetViewUp(), cam.GetViewAngle());
+        finalState.applyTo(cam);
     }
 
     // Set camera's focal point
     public void setCameraFocalPoint(double[] focalPoint)
     {
-        // Obtain lock
-        mainCanvas.getVTKLock().lock();
         vtkCamera cam = mainCanvas.getRenderer().GetActiveCamera();
-        cam.SetFocalPoint(focalPoint);
-        mainCanvas.getVTKLock().unlock();
-        mainCanvas.resetCameraClippingRange();
-        mainCanvas.Render();
-    }
+
+        CameraState finalState=new CameraState(cam.GetPosition(), focalPoint, cam.GetViewUp(), cam.GetViewAngle());
+        finalState.applyTo(cam);
+}
 
     // Sets the camera roll with roll as defined by vtkCamera
     public void setCameraRoll(double angle)
