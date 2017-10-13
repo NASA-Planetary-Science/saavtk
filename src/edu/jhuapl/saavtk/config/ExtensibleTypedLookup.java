698658b2e86f788501f962dd438920b9f1380f35
James Peachey <James.Peachey@jhuapl.edu>
2017-10-13 09:23:18 -0400

    ExtensibleTypedLookup.Builder now only builds and caches one instance.
    This involved making build() final and creating a new abstract doBuild()
    method for subclasses to implement.

--- src/edu/jhuapl/saavtk/config/ExtensibleTypedLookup.java
+++ src/edu/jhuapl/saavtk/config/ExtensibleTypedLookup.java
@@ -5,20 +5,34 @@ public abstract class ExtensibleTypedLookup implements TypedLookup
 	public static abstract class Builder<T extends ExtensibleTypedLookup> implements TypedLookup.TypedBuilder {
 
 		private final FixedTypedLookup.Builder fixedBuilder;
+		private T built;
 
 		protected Builder(FixedTypedLookup.Builder builder) {
 			if (builder == null) throw new NullPointerException();
 			this.fixedBuilder = builder;
+			this.built = null;
 		}
 
 		@Override
 		public <ValueType> Builder<T> put(Key<ValueType> key, ValueType value) {
+			if (built != null)
+			{
+				throw new UnsupportedOperationException("Cannot put a new entry in the map after it was already built");
+			}
 			fixedBuilder.put(key, value);
 			return this;
 		}
 
+		public abstract T doBuild();
+
 		@Override
-		public abstract T build();
+		public final T build() {
+			if (built == null)
+			{
+				built = doBuild();
+			}
+			return built;
+		}
 
 		protected FixedTypedLookup.Builder getFixedBuilder() {
 			return fixedBuilder;
