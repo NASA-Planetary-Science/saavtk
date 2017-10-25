6253a58ad88dbaa04c7f79581476a5d685cfc56b
James Peachey <James.Peachey@jhuapl.edu>
2017-10-25 16:14:08 -0400

    Add comments. Ensure no duplicate entries in map.

--- src/edu/jhuapl/saavtk/config/Key.java
+++ src/edu/jhuapl/saavtk/config/Key.java
@@ -1,11 +1,26 @@
 package edu.jhuapl.saavtk.config;
 
 /**
- * @param <ValueType>
- *            Type of value associated with this key.
+ * A key for accessing objects of a specific type in a map or configuration.
+ * Instances may be used as if they were enumerated constants, but without
+ * the need to limit values to a fixed set at compile-time. Since hashCode
+ * and equals just call the superclass (Object) implementation, and thus
+ * equals behaves as ==, instances should be declared as static final.
+ * 
+ * @author peachjm1
+ *
+ * @param <ValueType> the type of value that may be retrieved with this Key.
+ * Note that, while this parameter is not actually referenced in Key,
+ * it is essential so that Keys identify the type of object they may access.
  */
 public final class Key<ValueType>
 {
+	/**
+	 * Create a new key from the given identifier.
+	 * @param identifier cosmetic string identifier used for this key
+	 * @return the new key.
+	 * @throws NullPointerException if identifier is null
+	 */
 	public static <ValueType> Key<ValueType> of(String identifier)
 	{
 		return new Key<>(identifier);
@@ -20,6 +35,10 @@ public final class Key<ValueType>
 		this.identifier = identifier;
 	}
 
+	/**
+	 * Return this Key's identifier string.
+	 * @return the identifier
+	 */
 	public String getIdentifier()
 	{
 		return identifier;
