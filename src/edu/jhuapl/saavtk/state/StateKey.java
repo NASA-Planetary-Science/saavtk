dd80bdf853b7b55025da3fea5a61f8c36070e16f
James Peachey <James.Peachey@jhuapl.edu>
2018-04-18 10:10:22 -0400

    Add support for StateKey<List<?>>, including a secondary class.
    Most of this would work for Iterable more generally, or
    really any generic with one type argument whose argument
    is supported/supportable.

--- src/edu/jhuapl/saavtk/state/StateKey.java
+++ src/edu/jhuapl/saavtk/state/StateKey.java
@@ -1,69 +1,87 @@
 package edu.jhuapl.saavtk.state;
 
+import java.util.List;
+
 import com.google.common.base.Preconditions;
 
 public final class StateKey<V> implements Comparable<StateKey<?>>
 {
 	public static StateKey<State> ofState(String keyId)
 	{
-		return new StateKey<>(State.class, keyId);
+		return new StateKey<>(keyId, State.class, null);
+	}
+
+	public static <T> StateKey<List<T>> ofList(String keyId, Class<T> secondaryClass)
+	{
+		// Need to tap dance around Java's type erasure.
+		StateKey<?> genericKey = of(keyId, List.class, secondaryClass);
+		@SuppressWarnings("unchecked")
+		StateKey<List<T>> result = (StateKey<List<T>>) genericKey;
+		return result;
 	}
 
 	public static StateKey<String> ofString(String keyId)
 	{
-		return new StateKey<>(String.class, keyId);
+		return new StateKey<>(keyId, String.class, null);
 	}
 
 	public static StateKey<Integer> ofInteger(String keyId)
 	{
-		return new StateKey<>(Integer.class, keyId);
+		return new StateKey<>(keyId, Integer.class, null);
 	}
 
 	public static StateKey<Long> ofLong(String keyId)
 	{
-		return new StateKey<>(Long.class, keyId);
+		return new StateKey<>(keyId, Long.class, null);
 	}
 
 	public static StateKey<Short> ofShort(String keyId)
 	{
-		return new StateKey<>(Short.class, keyId);
+		return new StateKey<>(keyId, Short.class, null);
 	}
 
 	public static StateKey<Byte> ofByte(String keyId)
 	{
-		return new StateKey<>(Byte.class, keyId);
+		return new StateKey<>(keyId, Byte.class, null);
 	}
 
 	public static StateKey<Double> ofDouble(String keyId)
 	{
-		return new StateKey<>(Double.class, keyId);
+		return new StateKey<>(keyId, Double.class, null);
 	}
 
 	public static StateKey<Float> ofFloat(String keyId)
 	{
-		return new StateKey<>(Float.class, keyId);
+		return new StateKey<>(keyId, Float.class, null);
 	}
 
 	public static StateKey<Character> ofCharacter(String keyId)
 	{
-		return new StateKey<>(Character.class, keyId);
+		return new StateKey<>(keyId, Character.class, null);
 	}
 
 	public static StateKey<Boolean> ofBoolean(String keyId)
 	{
-		return new StateKey<>(Boolean.class, keyId);
+		return new StateKey<>(keyId, Boolean.class, null);
 	}
 
-	public static <V> StateKey<V> of(Class<V> valueClass, String keyId)
+	public static <V> StateKey<V> of(String keyId, Class<V> valueClass)
 	{
-		return new StateKey<>(valueClass, keyId);
+		return new StateKey<>(keyId, valueClass, null);
 	}
 
-	private final Class<V> valueClass;
+	public static <V> StateKey<V> of(String keyId, Class<V> valueClass, Class<?> secondaryClass)
+	{
+		return new StateKey<>(keyId, valueClass, secondaryClass);
+	}
+
 	private final String keyId;
+	private final Class<V> valueClass;
+	private final Class<?> secondaryClass;
 
-	private StateKey(Class<V> valueClass, String keyId)
+	private StateKey(String keyId, Class<V> valueClass, Class<?> secondaryClass)
 	{
+		this.secondaryClass = secondaryClass;
 		Preconditions.checkNotNull(valueClass);
 		Preconditions.checkNotNull(keyId);
 		Preconditions.checkArgument(!keyId.isEmpty());
@@ -77,6 +95,15 @@ public final class StateKey<V> implements Comparable<StateKey<?>>
 	public Class<V> getValueClass()
 	{
 		return valueClass;
+	}
+
+	public Class<?> getSecondaryClass()
+	{
+		if (secondaryClass == null)
+		{
+			throw new UnsupportedOperationException("Key " + keyId + " does not have a secondary class");
+		}
+		return secondaryClass;
 	}
 
 	public String getId()
@@ -140,6 +167,8 @@ public final class StateKey<V> implements Comparable<StateKey<?>>
 	private static final void checkClassValue(Class<?> valueClass)
 	{
 		if (State.class.isAssignableFrom(valueClass))
+			return;
+		if (List.class.isAssignableFrom(valueClass))
 			return;
 		if (String.class.isAssignableFrom(valueClass))
 			return;
