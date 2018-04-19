71d916116b37d1b03d40dc47c206655664deb5d8
James Peachey <James.Peachey@jhuapl.edu>
2018-04-19 14:49:37 -0400

    Add more test code, including a couple different types of List.

--- src/edu/jhuapl/saavtk/state/gson/GsonFileStateSerializer.java
+++ src/edu/jhuapl/saavtk/state/gson/GsonFileStateSerializer.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
@@ -80,23 +83,30 @@ public class GsonFileStateSerializer implements StateSerializer
 		v3State.put(StateKey.ofString("string"), "a string");
 		v3State.put(StateKey.ofString("stringNull"), null);
 		v3State.put(StateKey.ofLong("longNull"), null);
-		//		List<String> stringList = new ArrayList<>();
-		//		stringList.add("String0");
-		//		stringList.add(null);
-		//		stringList.add("String2");
-		//		StateKey<List<String>> stringListKey = StateKey.ofList("stringList", String.class);
-		//		v3State.put(stringListKey, stringList);
-		//
-		//		List<Integer> intList = new ArrayList<>();
-		//		intList.add(0);
-		//		intList.add(null);
-		//		intList.add(2);
-		//		StateKey<List<Integer>> intListKey = StateKey.ofList("intList", Integer.class);
-		//		v3State.put(intListKey, intList);
+
+		List<String> stringList = new ArrayList<>();
+		stringList.add("String0");
+		stringList.add(null);
+		stringList.add("String2");
+		StateKey<List<String>> stringListKey = StateKey.ofList("stringList", String.class);
+		v3State.put(stringListKey, stringList);
+
+		List<Integer> intList = new ArrayList<>();
+		intList.add(0);
+		intList.add(null);
+		intList.add(2);
+		StateKey<List<Integer>> intListKey = StateKey.ofList("intList", Integer.class);
+		v3State.put(intListKey, intList);
+
+		List<List<String>> listStringList = new ArrayList<>();
+		listStringList.add(null);
+		listStringList.add(ImmutableList.of("X", "y", "z"));
+		listStringList.add(stringList);
 
 		State state = State.of();
 		state.put(StateKey.ofState("Bennu / V3"), v3State);
 		state.put(StateKey.ofString("Current View"), v3);
+		state.put(StateKey.ofList("listStringList", ArrayList.class), listStringList);
 		System.out.println("Original state is: " + state);
 
 		StateSerializer serializer = of(new File("/Users/peachjm1/Downloads/MyState.sbmt"));
@@ -109,9 +119,11 @@ public class GsonFileStateSerializer implements StateSerializer
 			System.out.println("States are considered equal");
 		}
 		State v3State2 = state2.get(StateKey.ofState("Bennu / V3"));
-		Long nullLong = v3State2.get(StateKey.ofLong("longNull"));
-		System.out.println("nullLong is " + nullLong);
+		Long longNull = v3State2.get(StateKey.ofLong("longNull"));
+		System.out.println("longNull is " + longNull);
 		Float dVal = v3State2.get(StateKey.ofFloat("long"));
+		List<List<Integer>> unpackedListList = state2.get(StateKey.ofList("listStringList", List.class));
+		System.out.println(unpackedListList);
 		dVal = v3State2.get(StateKey.ofFloat("resolution"));
 		Short floatAsDouble = v3State2.get(StateKey.ofShort("facets"));
 		System.out.println("float as double is " + floatAsDouble);
