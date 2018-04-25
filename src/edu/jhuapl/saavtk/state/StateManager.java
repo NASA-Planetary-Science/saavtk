dc70c8a38ef23e419337df8512a9d813cb627e0f
James Peachey <James.Peachey@jhuapl.edu>
2018-04-25 18:51:49 -0400

    Add comments.

--- src/edu/jhuapl/saavtk/state/StateManager.java
+++ src/edu/jhuapl/saavtk/state/StateManager.java
@@ -1,9 +1,29 @@
 package edu.jhuapl.saavtk.state;
 
+/**
+ * Abstraction representing a manager of metadata, capable of storing/retrieving
+ * a collection of elements to/from a Metadata object. An implementation
+ * typically stores/retrieves all the information associated with the state of a
+ * particular Java object or class, or some other resource (e.g., a specific
+ * type of file or image, URI, URL, database query).
+ */
 public interface StateManager
 {
-	State getState();
+	/**
+	 * Store a (complete and self-consistent) set of metadata in the provided
+	 * destination object.
+	 * 
+	 * @param destination object in which the metadata are stored
+	 */
+	void store(State destination);
 
-	void setState(State state);
+	/**
+	 * Retrieve a (complete and self-consistent) set of metadata in the provided
+	 * source object. The metadata will typically be used to create or to modify the
+	 * state of some other type of object.
+	 * 
+	 * @param source the source of metadata
+	 */
+	void retrieve(State source);
 
 }
