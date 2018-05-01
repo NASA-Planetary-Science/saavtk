0c2f6cce6204b533215aabab4978291da4439dd5
James Peachey <James.Peachey@jhuapl.edu>
2018-05-01 12:45:25 -0400

    Add convenience method to serialize from MDManager to File.

--- src/edu/jhuapl/saavtk/metadata/Serializers.java
+++ src/edu/jhuapl/saavtk/metadata/Serializers.java
@@ -1,5 +1,8 @@
 package edu.jhuapl.saavtk.metadata;
 
+import java.io.File;
+import java.io.IOException;
+
 import edu.jhuapl.saavtk.metadata.gson.GsonSerializer;
 
 public class Serializers
@@ -14,6 +17,13 @@ public class Serializers
 	public static Serializer of()
 	{
 		return GsonSerializer.of();
+	}
+
+	public static void serialize(String metadataId, MetadataManager manager, File file) throws IOException
+	{
+		Serializer serializer = of();
+		serializer.register(Key.of(metadataId), manager);
+		serializer.save(file);
 	}
 
 }
