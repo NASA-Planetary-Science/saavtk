46ab7ce47fee2337c6d632817f0540005f7eba19
James Peachey <James.Peachey@jhuapl.edu>
2018-05-02 10:32:45 -0400

    Add method toColoringData and numberElements field.

--- src/edu/jhuapl/saavtk/model/ColoringInfo.java
+++ src/edu/jhuapl/saavtk/model/ColoringInfo.java
@@ -1,34 +1,42 @@
 package edu.jhuapl.saavtk.model;
 
-import vtk.vtkFloatArray;
+import com.google.common.collect.ImmutableList;
+
 import edu.jhuapl.saavtk.model.PolyhedralModel.Format;
+import vtk.vtkFloatArray;
 
 // Class storing info related to plate data used to color shape model
 public class ColoringInfo
 {
-    public String coloringName = null;
-    public String coloringUnits = null;
-    public boolean coloringHasNulls = false;
-    public int resolutionLevel = 0;
-    public double[] defaultColoringRange = null;
-    public double[] currentColoringRange = null;
-    public vtkFloatArray coloringValues = null;
-    public String coloringFile = null;
-    public boolean builtIn = true;
-    public Format format = Format.UNKNOWN;
+	public String coloringName = null;
+	public String coloringUnits = null;
+	public boolean coloringHasNulls = false;
+	public int resolutionLevel = 0;
+	public double[] defaultColoringRange = null;
+	public double[] currentColoringRange = null;
+	public vtkFloatArray coloringValues = null;
+	public String coloringFile = null;
+	public boolean builtIn = true;
+	public Format format = Format.UNKNOWN;
+	public int numberElements = -1;
 
-    @Override
-    public String toString()
-    {
-        String str = coloringName;
-        if (coloringUnits != null && !coloringUnits.isEmpty())
-            str += ", " + coloringUnits;
-        if (format != Format.TXT)
-            str += ", " + format.toString();
-        if (coloringHasNulls)
-            str += ", contains invalid data";
-        if (builtIn)
-            str += ", (built-in and cannot be modified)";
-        return str;
-    }
+	ColoringData toColoringData()
+	{
+		return ColoringData.of(coloringName, coloringFile, format, ImmutableList.of(coloringName), coloringUnits, numberElements, coloringHasNulls);
+	}
+
+	@Override
+	public String toString()
+	{
+		String str = coloringName;
+		if (coloringUnits != null && !coloringUnits.isEmpty())
+			str += ", " + coloringUnits;
+		if (format != Format.TXT)
+			str += ", " + format.toString();
+		if (coloringHasNulls)
+			str += ", contains invalid data";
+		if (builtIn)
+			str += ", (built-in and cannot be modified)";
+		return str;
+	}
 }
