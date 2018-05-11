74ceb97f61405315affee508513dd40cd11d8f4e
James Peachey <James.Peachey@jhuapl.edu>
2018-05-11 09:38:27 -0400

    Add some convenience methods for quick load/save support.

--- src/edu/jhuapl/saavtk/metadata/Serializers.java
+++ src/edu/jhuapl/saavtk/metadata/Serializers.java
@@ -26,4 +26,63 @@ public class Serializers
 		serializer.save(file);
 	}
 
+	public static void serialize(String metadataId, Metadata metadata, File file) throws IOException
+	{
+		serialize(metadataId, new MetadataManager() {
+
+			@Override
+			public Metadata store()
+			{
+				return metadata;
+			}
+
+			@Override
+			public void retrieve(Metadata source)
+			{
+				throw new UnsupportedOperationException();
+			}
+
+		}, file);
+	}
+
+	public static void deserialize(File file, String metadataId, MetadataManager manager) throws IOException
+	{
+		Serializer serializer = of();
+		serializer.register(Key.of(metadataId), manager);
+		serializer.load(file);
+	}
+
+	public static FixedMetadata deserialize(File file, String metadataId) throws IOException
+	{
+		class DirectMetadataManager implements MetadataManager
+		{
+			final SettableMetadata metadata = SettableMetadata.of(Version.of(0, 1));
+
+			@Override
+			public Metadata store()
+			{
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public void retrieve(Metadata source)
+			{
+				for (Key<?> key : source.getKeys())
+				{
+					put(key, source.get(key), metadata);
+				}
+			}
+
+		}
+
+		DirectMetadataManager manager = new DirectMetadataManager();
+		deserialize(file, metadataId, manager);
+		return FixedMetadata.of(manager.metadata);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> void put(Key<?> key, T object, SettableMetadata metadata)
+	{
+		metadata.put((Key<T>) key, object);
+	}
 }
