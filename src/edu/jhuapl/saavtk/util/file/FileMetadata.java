323a397826c4b893e24b20f2fa07e954d49a10da
James Peachey <James.Peachey@jhuapl.edu>
2018-05-26 18:01:22 -0400

    Reworked to produce metadata compatible with MetadataDisplayPanel

--- src/edu/jhuapl/saavtk/util/file/FileMetadata.java
+++ src/edu/jhuapl/saavtk/util/file/FileMetadata.java
@@ -2,11 +2,13 @@ package edu.jhuapl.saavtk.util.file;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
+import edu.jhuapl.saavtk.gui.MetadataDisplayPanel;
 import edu.jhuapl.saavtk.metadata.FixedMetadata;
 import edu.jhuapl.saavtk.metadata.Key;
 import edu.jhuapl.saavtk.metadata.Serializers;
@@ -15,11 +17,26 @@ import edu.jhuapl.saavtk.metadata.Version;
 import nom.tam.fits.BasicHDU;
 import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
-import nom.tam.fits.TableHDU;
+import nom.tam.fits.Header;
+import nom.tam.fits.HeaderCard;
+import nom.tam.util.Cursor;
 
 public class FileMetadata
 {
-	public static final Key<List<FixedMetadata>> TABLES = Key.of("Tables");
+	public static final Version VERSION = Version.of(0, 1);
+
+	// Data object (FITS: list of HDUs in the file).
+	public static final Key<List<FixedMetadata>> DATA_OBJECTS = Key.of("Data objects"); // Each has DESCRIPTION_FIELDS, DESCRIPTION
+
+	// Key-value pairs in this metadata must be displayable, i.e., its keys are Key<List<?>>.
+	public static final Key<FixedMetadata> DESCRIPTION = Key.of("Data object description");
+
+	// Fields used to describe the data object.
+	public static final Key<List<String>> DESCRIPTION_FIELDS = Key.of("Fields"); // Meta-metadata that tells us what is in the description.
+
+	// These are the fields from a FITS keyword other than the name of the keyword.
+	public static final List<String> FITS_KEYWORD_FIELDS = ImmutableList.of("Value", "Comment");
+
 	public static final Key<List<FixedMetadata>> COLUMNS = Key.of("Columns");
 	public static final Key<Integer> NUMBER_RECORDS = Key.of("Number of records");
 	public static final Key<String> COLUMN_NAME = Key.of("Column name");
@@ -32,12 +49,6 @@ public class FileMetadata
 	public static FileMetadata of()
 	{
 		return INSTANCE;
-	}
-
-	public enum Format
-	{
-		FITS,
-		TEXT,;
 	}
 
 	private FileMetadata(Version version)
@@ -79,17 +90,21 @@ public class FileMetadata
 				ImmutableList.Builder<FixedMetadata> builder = ImmutableList.builder();
 				for (BasicHDU<?> hdu : fits.read())
 				{
-					if (hdu instanceof TableHDU)
-					{
-						TableHDU<?> tableHdu = (TableHDU<?>) hdu;
-						builder.add(tableSummary(tableHdu));
-					}
-					else
-					{
-						// TODO handle image metadata too.
-					}
+					SettableMetadata hduMetadata = SettableMetadata.of(version);
+					addFitsKeywords(hdu, hduMetadata);
+
+					//					if (hdu instanceof TableHDU)
+					//					{
+					//						TableHDU<?> tableHdu = (TableHDU<?>) hdu;
+					//						builder.add(tableSummary(tableHdu));
+					//					}
+					//					else
+					//					{
+					//						// TODO handle image metadata too.
+					//					}
+					builder.add(FixedMetadata.of(hduMetadata));
 				}
-				metadata.put(TABLES, builder.build());
+				metadata.put(DATA_OBJECTS, builder.build());
 				result = FixedMetadata.of(metadata);
 			}
 			catch (FitsException e)
@@ -106,33 +121,51 @@ public class FileMetadata
 		return result;
 	}
 
+	private void addFitsKeywords(BasicHDU<?> hdu, SettableMetadata hduMetadata)
+	{
+		hduMetadata.put(DESCRIPTION_FIELDS, FITS_KEYWORD_FIELDS);
+
+		SettableMetadata keywordMetadata = SettableMetadata.of(VERSION);
+		Header header = hdu.getHeader();
+		Cursor<String, HeaderCard> iterator = header.iterator();
+		while (iterator.hasNext())
+		{
+			HeaderCard card = iterator.next();
+			ArrayList<String> valueAndComment = new ArrayList<>();
+			valueAndComment.add(card.getValue());
+			valueAndComment.add(card.getComment());
+			keywordMetadata.put(Key.of(card.getKey()), valueAndComment);
+		}
+		hduMetadata.put(DESCRIPTION, FixedMetadata.of(keywordMetadata));
+	}
+
 	private FixedMetadata textSummary(File file) throws IOException
 	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	private FixedMetadata tableSummary(TableHDU<?> tableHdu) throws IOException
-	{
-		SettableMetadata metadata = SettableMetadata.of(version);
-		ImmutableList.Builder<FixedMetadata> builder = ImmutableList.builder();
-
-		for (int index = 0; index < tableHdu.getNCols(); ++index)
-		{
-			builder.add(columnSummary(tableHdu, index));
-		}
-		metadata.put(NUMBER_RECORDS, tableHdu.getNRows());
-		metadata.put(COLUMNS, builder.build());
-		return FixedMetadata.of(metadata);
-	}
-
-	private FixedMetadata columnSummary(TableHDU<?> tableHdu, int index)
-	{
-		SettableMetadata metadata = SettableMetadata.of(version);
-		metadata.put(COLUMN_NAME, tableHdu.getColumnMeta(index, "TTYPE"));
-		metadata.put(UNITS, tableHdu.getColumnMeta(index, "TUNIT"));
-		return FixedMetadata.of(metadata);
-	}
+	//	private FixedMetadata tableSummary(TableHDU<?> tableHdu) throws IOException
+	//	{
+	//		SettableMetadata metadata = SettableMetadata.of(version);
+	//		ImmutableList.Builder<FixedMetadata> builder = ImmutableList.builder();
+	//
+	//		for (int index = 0; index < tableHdu.getNCols(); ++index)
+	//		{
+	//			builder.add(columnSummary(tableHdu, index));
+	//		}
+	//		metadata.put(NUMBER_RECORDS, tableHdu.getNRows());
+	//		metadata.put(COLUMNS, builder.build());
+	//		return FixedMetadata.of(metadata);
+	//	}
+	//
+	//	private FixedMetadata columnSummary(TableHDU<?> tableHdu, int index)
+	//	{
+	//		SettableMetadata metadata = SettableMetadata.of(version);
+	//		metadata.put(COLUMN_NAME, tableHdu.getColumnMeta(index, "TTYPE"));
+	//		metadata.put(UNITS, tableHdu.getColumnMeta(index, "TUNIT"));
+	//		return FixedMetadata.of(metadata);
+	//	}
 
 	public static void main(String[] args)
 	{
@@ -143,6 +176,7 @@ public class FileMetadata
 		{
 			FixedMetadata metadata = fileMetadata.summary(fitsFile);
 			Serializers.serialize("Slope_res0.fits", metadata, metadataFile);
+			display(metadata);
 			metadata = Serializers.deserialize(metadataFile, "Slope_res0.fits");
 			System.out.println(metadata);
 		}
@@ -150,6 +184,17 @@ public class FileMetadata
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	private static void display(FixedMetadata fileMetadata)
+	{
+		List<FixedMetadata> dataObjects = fileMetadata.get(DATA_OBJECTS);
+		for (FixedMetadata metadata : dataObjects)
+		{
+			List<String> fields = metadata.get(DESCRIPTION_FIELDS);
+			FixedMetadata description = metadata.get(DESCRIPTION);
+			MetadataDisplayPanel.display(description, "Keyword", fields);
 		}
 	}
 }
