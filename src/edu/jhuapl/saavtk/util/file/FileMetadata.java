06f4f16cc0a29979a2dae121c25a067988cc5fb6
James Peachey <James.Peachey@jhuapl.edu>
2018-06-11 07:35:30 -0400

    Prefer Metadata interface wherever possible.

--- src/edu/jhuapl/saavtk/util/file/FileMetadata.java
+++ src/edu/jhuapl/saavtk/util/file/FileMetadata.java
@@ -14,6 +14,7 @@ import com.google.common.collect.ImmutableList;
 import edu.jhuapl.saavtk.gui.MetadataDisplayPanel;
 import edu.jhuapl.saavtk.metadata.FixedMetadata;
 import edu.jhuapl.saavtk.metadata.Key;
+import edu.jhuapl.saavtk.metadata.Metadata;
 import edu.jhuapl.saavtk.metadata.Serializers;
 import edu.jhuapl.saavtk.metadata.SettableMetadata;
 import edu.jhuapl.saavtk.metadata.Version;
@@ -204,7 +205,7 @@ public class FileMetadata
 		}
 	}
 
-	private static void display(FixedMetadata fileMetadata)
+	private static void display(Metadata fileMetadata)
 	{
 		JTabbedPane jTabbedPane = new JTabbedPane();
 		List<FixedMetadata> dataObjects = fileMetadata.get(DATA_OBJECTS);
