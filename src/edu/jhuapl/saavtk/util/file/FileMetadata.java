e5763800b23f230fb70a6f334f5179687f847f95
James Peachey <James.Peachey@jhuapl.edu>
2018-06-11 20:44:31 -0400

    Use FileReader hierarchy rather than direct file access.

--- src/edu/jhuapl/saavtk/util/file/FileMetadata.java
+++ src/edu/jhuapl/saavtk/util/file/FileMetadata.java
@@ -2,13 +2,11 @@ package edu.jhuapl.saavtk.util.file;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
 import edu.jhuapl.saavtk.gui.MetadataDisplayPanel;
@@ -16,14 +14,8 @@ import edu.jhuapl.saavtk.metadata.FixedMetadata;
 import edu.jhuapl.saavtk.metadata.Key;
 import edu.jhuapl.saavtk.metadata.Metadata;
 import edu.jhuapl.saavtk.metadata.Serializers;
-import edu.jhuapl.saavtk.metadata.SettableMetadata;
 import edu.jhuapl.saavtk.metadata.Version;
-import nom.tam.fits.BasicHDU;
-import nom.tam.fits.Fits;
-import nom.tam.fits.FitsException;
-import nom.tam.fits.Header;
-import nom.tam.fits.HeaderCard;
-import nom.tam.util.Cursor;
+import edu.jhuapl.saavtk.util.file.FileReader.IncorrectFileFormatException;
 
 public class FileMetadata
 {
@@ -48,141 +40,24 @@ public class FileMetadata
 	public static final Key<String> COLUMN_NAME = Key.of("Column name");
 	public static final Key<String> UNITS = Key.of("Units");
 
-	private static final FileMetadata INSTANCE = new FileMetadata(Version.of(0, 1));
-
-	private final Version version;
+	private static final FileMetadata INSTANCE = new FileMetadata();
 
 	public static FileMetadata of()
 	{
 		return INSTANCE;
 	}
 
-	private FileMetadata(Version version)
-	{
-		this.version = version;
-	}
-
 	public FixedMetadata summary(File file) throws IOException
 	{
-		Preconditions.checkNotNull(file);
-		if (!file.exists())
+		try
 		{
-			throw new IOException("File does not exist: " + file);
+			return FixedMetadata.of(FileReader.of().readMetadata(file));
 		}
-
-		FixedMetadata result = fitsSummary(file);
-
-		if (result == null)
+		catch (IncorrectFileFormatException e)
 		{
-			result = textSummary(file);
+			throw new IOException(e);
 		}
-
-		if (result == null)
-		{
-			throw new IOException("File has unknown format: " + file.toString());
-		}
-
-		return result;
 	}
-
-	private FixedMetadata fitsSummary(File file) throws IOException
-	{
-		FixedMetadata result = null;
-		try (Fits fits = new Fits(file))
-		{
-			try
-			{
-				SettableMetadata metadata = SettableMetadata.of(version);
-				ImmutableList.Builder<FixedMetadata> builder = ImmutableList.builder();
-
-				BasicHDU<?>[] hdus = fits.read();
-				for (int hduNum = 0; hduNum < hdus.length; ++hduNum)
-				{
-					SettableMetadata hduMetadata = SettableMetadata.of(version);
-					addFitsKeywords(hduNum, hdus[hduNum], hduMetadata);
-					//					if (hdu instanceof TableHDU)
-					//					{
-					//						TableHDU<?> tableHdu = (TableHDU<?>) hdu;
-					//						builder.add(tableSummary(tableHdu));
-					//					}
-					//					else
-					//					{
-					//						// TODO handle image metadata too.
-					//					}
-					builder.add(FixedMetadata.of(hduMetadata));
-				}
-				metadata.put(DATA_OBJECTS, builder.build());
-				result = FixedMetadata.of(metadata);
-			}
-			catch (FitsException e)
-			{
-				// This means there is a problem with this FITS file.
-				throw new IOException(e);
-			}
-		}
-		catch (@SuppressWarnings("unused") FitsException e)
-		{
-			// Ignore an exception thrown when opening the file; it probably
-			// just means the file is not a FITS file.
-		}
-		return result;
-	}
-
-	private void addFitsKeywords(int hduNum, BasicHDU<?> hdu, SettableMetadata hduMetadata)
-	{
-
-		SettableMetadata keywordMetadata = SettableMetadata.of(VERSION);
-		Header header = hdu.getHeader();
-
-		// Derive the title.
-		String extName = header.getStringValue("EXTNAME");
-		boolean isPrimary = extName == null && header.getBooleanValue("SIMPLE");
-		final String title = extName != null ? extName : isPrimary ? "Primary" : "HDU " + hduNum;
-		hduMetadata.put(TITLE, title);
-
-		// Add boilerplate FITS meta-metadata.
-		hduMetadata.put(DESCRIPTION_FIELDS, FITS_KEYWORD_FIELDS);
-
-		// Get all the keywords.
-		Cursor<String, HeaderCard> iterator = header.iterator();
-		while (iterator.hasNext())
-		{
-			HeaderCard card = iterator.next();
-			ArrayList<String> valueAndComment = new ArrayList<>();
-			valueAndComment.add(card.getValue());
-			valueAndComment.add(card.getComment());
-			keywordMetadata.put(Key.of(card.getKey()), valueAndComment);
-		}
-
-		hduMetadata.put(DESCRIPTION, FixedMetadata.of(keywordMetadata));
-	}
-
-	private FixedMetadata textSummary(File file) throws IOException
-	{
-		throw new IOException("CSV files do not have summary metadata for now.");
-	}
-
-	//	private FixedMetadata tableSummary(TableHDU<?> tableHdu) throws IOException
-	//	{
-	//		SettableMetadata metadata = SettableMetadata.of(version);
-	//		ImmutableList.Builder<FixedMetadata> builder = ImmutableList.builder();
-	//
-	//		for (int index = 0; index < tableHdu.getNCols(); ++index)
-	//		{
-	//			builder.add(columnSummary(tableHdu, index));
-	//		}
-	//		metadata.put(NUMBER_RECORDS, tableHdu.getNRows());
-	//		metadata.put(COLUMNS, builder.build());
-	//		return FixedMetadata.of(metadata);
-	//	}
-	//
-	//	private FixedMetadata columnSummary(TableHDU<?> tableHdu, int index)
-	//	{
-	//		SettableMetadata metadata = SettableMetadata.of(version);
-	//		metadata.put(COLUMN_NAME, tableHdu.getColumnMeta(index, "TTYPE"));
-	//		metadata.put(UNITS, tableHdu.getColumnMeta(index, "TUNIT"));
-	//		return FixedMetadata.of(metadata);
-	//	}
 
 	// Test code below here.
 	public static void main(String[] args)
