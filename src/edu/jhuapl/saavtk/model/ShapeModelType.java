a4380fbbdb2edb6b8307e1d0297301138ce39d66
James Peachey <James.Peachey@jhuapl.edu>
2018-06-28 22:40:16 -0400

    Replace the Hayabusa2 type with JAXA-001 and NASA-001.

--- src/edu/jhuapl/saavtk/model/ShapeModelType.java
+++ src/edu/jhuapl/saavtk/model/ShapeModelType.java
@@ -3,36 +3,38 @@ package edu.jhuapl.saavtk.model;
 // Names of authors
 public enum ShapeModelType
 {
-    GASKELL("Gaskell"),
-    THOMAS("Thomas"),
-    STOOKE("Stooke"),
-    HUDSON("Hudson"),
-    DUXBURY("Duxbury"),
-    OSTRO("Ostro"),
-    JORDA("Jorda"),
-    NOLAN("Nolan"),
-    CUSTOM("Custom"),
-    EROSNAV("NAV"),
-    EROSNLR("NLR"),
-    EXPERIMENTAL("Experimental"),
-    LORRI("LORRI"),
-    MVIC("MVIC"),
-    CARRY("Carry"),
-    DLR("DLR"),
-    BLENDER("Zimmerman"),
-    HAYABUSA2("Hayabusa2-testing"),
-    OREX("OSIRIS-REx"),
-    TRUTH("Truth");
+	GASKELL("Gaskell"),
+	THOMAS("Thomas"),
+	STOOKE("Stooke"),
+	HUDSON("Hudson"),
+	DUXBURY("Duxbury"),
+	OSTRO("Ostro"),
+	JORDA("Jorda"),
+	NOLAN("Nolan"),
+	CUSTOM("Custom"),
+	EROSNAV("NAV"),
+	EROSNLR("NLR"),
+	EXPERIMENTAL("Experimental"),
+	LORRI("LORRI"),
+	MVIC("MVIC"),
+	CARRY("Carry"),
+	DLR("DLR"),
+	BLENDER("Zimmerman"),
+	JAXA_001("JAXA-001"),
+	NASA_001("NASA-001"),
+	OREX("OSIRIS-REx"),
+	TRUTH("Truth");
 
-    final private String str;
-    private ShapeModelType(String str)
-    {
-        this.str = str;
-    }
+	final private String str;
 
-    @Override
-    public String toString()
-    {
-        return str;
-    }
+	private ShapeModelType(String str)
+	{
+		this.str = str;
+	}
+
+	@Override
+	public String toString()
+	{
+		return str;
+	}
 }
