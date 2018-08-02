ee642fa21a0ffd5045edc618cb3597d32df99bdf
James Peachey <James.Peachey@jhuapl.edu>
2018-08-02 11:23:12 -0400

    Use column identifiers and save metadata file.

--- src/edu/jhuapl/saavtk/gui/dialog/CustomPlateDataDialog.java
+++ src/edu/jhuapl/saavtk/gui/dialog/CustomPlateDataDialog.java
@@ -167,6 +167,16 @@ public class CustomPlateDataDialog extends javax.swing.JDialog
 		newMap.put(PolyhedralModel.CELL_DATA_RESOLUTION_LEVEL, cellDataResolutionLevels);
 
 		configMap.put(newMap);
+		try
+		{
+			coloringDataManager.saveCustomMetadata(modelManager.getPolyhedralModel().getCustomDataFolder());
+		}
+		catch (IOException e)
+		{
+			// This should not fail, but if it does it should not disrupt what the user is doing.
+			// Thus in this case it is appropriate to log the problem and then continue.
+			e.printStackTrace();
+		}
 	}
 
 	private ColoringData copyCellData(int index, ColoringData source)
@@ -191,7 +201,7 @@ public class CustomPlateDataDialog extends javax.swing.JDialog
 
 		// After copying the file, convert the file path to a URL format.
 		destFilePath = FileCache.FILE_PREFIX + getCustomDataFolder() + "/" + destFileName;
-		ColoringData newColoringData = ColoringData.of(source.getName(), destFilePath, source.getElementNames(), source.getUnits(), source.getNumberElements(), source.hasNulls());
+		ColoringData newColoringData = ColoringData.of(source.getName(), destFilePath, source.getElementNames(), source.getColumnIdentifiers(), source.getUnits(), source.getNumberElements(), source.hasNulls());
 
 		DefaultListModel<ColoringData> model = (DefaultListModel<ColoringData>) cellDataList.getModel();
 		if (index >= model.getSize())
