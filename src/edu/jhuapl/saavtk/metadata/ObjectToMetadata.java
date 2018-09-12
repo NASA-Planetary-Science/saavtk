b0c50e4bab1116cfbd2a57945a6825c65780bbd0
James Peachey <James.Peachey@jhuapl.edu>
2018-09-12 07:44:58 -0400

    Refine a few comments.

--- src/edu/jhuapl/saavtk/metadata/ObjectToMetadata.java
+++ src/edu/jhuapl/saavtk/metadata/ObjectToMetadata.java
@@ -1,16 +1,16 @@
 package edu.jhuapl.saavtk.metadata;
 
 /**
- * Abstraction that may be converted to (proxied as) {@link Metadata}.
+ * Abstraction that may be converted to (stored as) {@link Metadata}.
  * 
  * @param <T> the object type that can be converted to Metadata
  */
 public interface ObjectToMetadata<T>
 {
 	/**
-	 * Return a key that uniquely identifies the type of object being proxied. This
+	 * Return a key that uniquely identifies the type of object being stored. This
 	 * is used in lieu of the object's Class to identify the type for purposes of
-	 * saving/restoring the {@link Metadata}.
+	 * saving the {@link Metadata}.
 	 * 
 	 * @return the key
 	 * @see {@link MetadataToObject}
