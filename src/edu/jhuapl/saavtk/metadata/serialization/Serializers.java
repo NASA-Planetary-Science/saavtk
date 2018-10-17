c76a1d377b2d17e09cf84da79a6f55d6adc1ca23
James Peachey <James.Peachey@jhuapl.edu>
2018-10-17 19:22:32 -0400

    Move new helper methods to a utilities class.
    These didn't really fit in Serializers.

--- src/edu/jhuapl/saavtk/metadata/serialization/Serializers.java
+++ src/edu/jhuapl/saavtk/metadata/serialization/Serializers.java
@@ -2,11 +2,8 @@ package edu.jhuapl.saavtk.metadata.serialization;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
 
 import edu.jhuapl.saavtk.metadata.FixedMetadata;
 import edu.jhuapl.saavtk.metadata.Key;
@@ -107,33 +104,6 @@ public class Serializers
 		DirectMetadataManager manager = new DirectMetadataManager();
 		deserialize(file, metadataId, manager);
 		return FixedMetadata.of(manager.metadata);
-	}
-
-	public static <K> ImmutableMap<K, Metadata> bulkStore(Map<K, MetadataManager> managers)
-	{
-		Preconditions.checkNotNull(managers);
-
-		ImmutableMap.Builder<K, Metadata> builder = ImmutableMap.builder();
-		for (Entry<K, MetadataManager> entry : managers.entrySet())
-		{
-			builder.put(entry.getKey(), entry.getValue().store());
-		}
-
-		return builder.build();
-	}
-
-	public static <K> void bulkRetrieve(Map<K, MetadataManager> managers, Map<K, Metadata> metadata)
-	{
-		Preconditions.checkNotNull(managers);
-		Preconditions.checkNotNull(metadata);
-		for (Entry<K, MetadataManager> entry : managers.entrySet())
-		{
-			K key = entry.getKey();
-			if (metadata.containsKey(key))
-			{
-				entry.getValue().retrieve(metadata.get(key));
-			}
-		}
 	}
 
 	@SuppressWarnings("unchecked")
