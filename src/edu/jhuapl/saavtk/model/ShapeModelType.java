af7dfc624f6cde91e52745cde6f7a5eeccaa2f5d
James Peachey <James.Peachey@jhuapl.edu>
2018-11-21 00:05:07 -0500

    RM 1552: add ALTWG-SPC-v20181115 model.

--- src/edu/jhuapl/saavtk/model/ShapeModelType.java
+++ src/edu/jhuapl/saavtk/model/ShapeModelType.java
@@ -64,6 +64,7 @@ public final class ShapeModelType
 	public static final ShapeModelType OREX = create("OSIRIS-REx");
 	public static final ShapeModelType TRUTH = create("Truth");
 	public static final ShapeModelType ALTWG_SPC_v20181109b = create("ALTWG-SPC-v20181109b");
+	public static final ShapeModelType ALTWG_SPC_v20181115 = create("ALTWG-SPC-v20181115");
 
 	private final String identifier;
 
