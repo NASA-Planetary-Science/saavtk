5be763bc504d1127e3b6fe6763f6f57e0a318bed
James Peachey <James.Peachey@jhuapl.edu>
2018-11-27 11:00:07 -0500

    RM #1558: add ALTWG-SPC-v20181116.

--- src/edu/jhuapl/saavtk/model/ShapeModelType.java
+++ src/edu/jhuapl/saavtk/model/ShapeModelType.java
@@ -65,6 +65,7 @@ public final class ShapeModelType
 	public static final ShapeModelType TRUTH = create("Truth");
 	public static final ShapeModelType ALTWG_SPC_v20181109b = create("ALTWG-SPC-v20181109b");
 	public static final ShapeModelType ALTWG_SPC_v20181115 = create("ALTWG-SPC-v20181115");
+	public static final ShapeModelType ALTWG_SPC_v20181116 = create("ALTWG-SPC-v20181116");
 
 	private final String identifier;
 
