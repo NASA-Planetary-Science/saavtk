581e777f090128e8f3f192b718d18b86398eb5b6
James Peachey <James.Peachey@jhuapl.edu>
2019-05-02 13:16:59 -0400

    New cleaner underpinnings for FileCache.

---
+++ src/edu/jhuapl/saavtk/util/FileCacheManager.java
@@ -0,0 +1,61 @@
+package edu.jhuapl.saavtk.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import com.google.common.base.Preconditions;
+
+public class FileCacheManager
+{
+    private static final SafeURLPaths SAFE_URL_PATHS = SafeURLPaths.instance();
+
+    public static FileCacheManager of(File cacheRoot) throws IOException
+    {
+        Preconditions.checkNotNull(cacheRoot);
+        Preconditions.checkArgument(cacheRoot.isDirectory() || !cacheRoot.exists());
+
+        if (!cacheRoot.exists())
+        {
+            if (!cacheRoot.mkdirs())
+            {
+                throw new IOException("Unable to create local file cache directory " + cacheRoot);
+            }
+        }
+
+        return new FileCacheManager(cacheRoot.toPath());
+    }
+
+    private final Path cacheRoot;
+
+    protected FileCacheManager(Path cacheRoot)
+    {
+        this.cacheRoot = cacheRoot;
+    }
+
+    public boolean isAccessible(String fileIdentifier)
+    {
+        Preconditions.checkNotNull(fileIdentifier);
+
+        return getFile(fileIdentifier).exists();
+    }
+
+    public File getFile(String fileIdentifier)
+    {
+//        Path path = SAFE_URL_PATHS.get(fileIdentifier);
+//        Path absolutePath = path.toAbsolutePath();
+//
+//        if (!path.equals(absolutePath))
+//        {
+//            // The argument supplied a relative path. Resolve it against the
+//            // root directory of the cache.
+//            path = cacheRoot.resolve(fileIdentifier);
+//        }
+//
+//        return path.toFile();
+        Path filePath = SAFE_URL_PATHS.get(cacheRoot.toString(), fileIdentifier);
+
+        return filePath.toFile();
+    }
+
+}
