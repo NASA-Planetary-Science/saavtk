22c7133a88b0387aa8fcb6e6c6be72a5f62ef925
James Peachey <James.Peachey@jhuapl.edu>
2019-05-13 15:08:27 -0400

    Rename and re-pattern after UrlAccessManager.

--- src/edu/jhuapl/saavtk/util/FileAccessManager.java
+++ src/edu/jhuapl/saavtk/util/FileAccessManager.java
@@ -3,48 +3,151 @@ package edu.jhuapl.saavtk.util;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import com.google.common.base.Preconditions;
 
-public class FileCacheManager
+import edu.jhuapl.saavtk.util.FileInfo.FileState;
+import edu.jhuapl.saavtk.util.FileInfo.FileStatus;
+
+public class FileAccessManager
 {
     private static final SafeURLPaths SAFE_URL_PATHS = SafeURLPaths.instance();
 
-    public static FileCacheManager of(File cacheRoot) throws IOException
+    public static FileAccessManager of(File rootDirectory) throws IOException
     {
-        Preconditions.checkNotNull(cacheRoot);
-        Preconditions.checkArgument(cacheRoot.isDirectory() || !cacheRoot.exists());
+        Preconditions.checkNotNull(rootDirectory);
+        Preconditions.checkArgument(rootDirectory.isDirectory() || !rootDirectory.exists());
 
-        if (!cacheRoot.exists())
+        if (!rootDirectory.exists())
         {
-            if (!cacheRoot.mkdirs())
+            if (!rootDirectory.mkdirs())
             {
-                throw new IOException("Unable to create local file cache directory " + cacheRoot);
+                throw new IOException("Unable to create local file cache directory " + rootDirectory);
             }
         }
 
-        return new FileCacheManager(cacheRoot.toPath());
+        return new FileAccessManager(rootDirectory.getCanonicalFile());
     }
 
-    private final Path cacheRoot;
+    private final File rootDirectory;
+    private final ConcurrentMap<File, FileInfo> fileInfoCache;
 
-    protected FileCacheManager(Path cacheRoot)
+    protected FileAccessManager(File rootPath)
     {
-        this.cacheRoot = cacheRoot;
+        this.rootDirectory = rootPath;
+        this.fileInfoCache = new ConcurrentHashMap<>();
     }
 
-    public boolean isAccessible(String fileIdentifier)
+    public File getRootDirectory()
     {
-        Preconditions.checkNotNull(fileIdentifier);
-
-        return getFile(fileIdentifier).exists();
+        return rootDirectory;
     }
 
-    public File getFile(String fileIdentifier)
+    public File getFile(Path path)
     {
-        Path filePath = SAFE_URL_PATHS.get(cacheRoot.toString(), fileIdentifier);
+        Preconditions.checkNotNull(path);
 
-        return filePath.toFile();
+        return getFile(path.toString());
     }
 
+    public File getFile(String pathString)
+    {
+        Preconditions.checkNotNull(pathString);
+
+        // Most likely the file is located relative to the root directory.
+        File file = SAFE_URL_PATHS.get(getRootDirectory().getPath(), pathString).toFile().getAbsoluteFile();
+
+        // However, if the supplied string already includes the root, need to prevent
+        // appending it again.
+        File rootDirectory = getRootDirectory();
+        if (pathString.startsWith(rootDirectory.toString()))
+        {
+            // The path string must start out like a canonical absolute path since it
+            // started with the root directory.
+            File absoluteFile = new File(pathString).getAbsoluteFile();
+
+            // Make sure the root directory really is an ancestor, i.e., that there wasn't
+            // just some partial lexical match.
+            File parent = absoluteFile;
+            while (parent != null && !rootDirectory.equals(parent))
+            {
+                parent = parent.getParentFile();
+            }
+            if (rootDirectory.equals(parent))
+            {
+                file = absoluteFile;
+            }
+        }
+
+        return file;
+    }
+
+    public FileInfo getInfo(Path path)
+    {
+        return getInfo(getFile(path));
+    }
+
+    public FileInfo getInfo(String pathString)
+    {
+        return getInfo(getFile(pathString));
+    }
+
+    public FileInfo getInfo(File file)
+    {
+        Preconditions.checkNotNull(file);
+
+        FileInfo result = fileInfoCache.get(file);
+        if (result == null)
+        {
+            result = FileInfo.of(file);
+            fileInfoCache.put(file, result);
+        }
+
+        return result;
+    }
+
+    public FileInfo queryFileSystem(String pathString, boolean forceUpdate)
+    {
+        return queryFileSystem(getFile(pathString), forceUpdate);
+    }
+
+    public FileInfo queryFileSystem(File file, boolean forceUpdate)
+    {
+        Preconditions.checkNotNull(file);
+
+        FileInfo result = getInfo(file);
+        FileState state = result.getState();
+        if (state.getStatus() == FileStatus.UNKNOWN || forceUpdate)
+        {
+            result.update();
+        }
+
+        return result;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "FileAccessManager(" + rootDirectory + ")";
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            FileAccessManager manager = of(new File(System.getProperty("user.home")));
+
+            System.err.println(manager.getFile("spud/junk.txt"));
+            System.err.println(manager.getFile("/spud/junk.txt"));
+            System.err.println(manager.getFile(manager.getRootDirectory().getPath()));
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ src/edu/jhuapl/saavtk/util/FileInfo.java
@@ -0,0 +1,186 @@
+package edu.jhuapl.saavtk.util;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.File;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Collect and store essential properties associated with a local file resource.
+ * These properties are useful for determining whether it is necessary and/or
+ * possible to download a remote file, or to determine the size of the file.
+ */
+public class FileInfo
+{
+    public static final String STATE_PROPERTY = "fileInfoState";
+
+    public enum FileStatus
+    {
+        ACCESSIBLE,
+        INACCESSIBLE,
+        UNKNOWN,
+        ;
+    }
+
+    public static class FileState
+    {
+        public static FileState of(File file)
+        {
+            return of(file, FileStatus.UNKNOWN, -1, 0);
+        }
+
+        public static FileState of(File file, FileStatus status, long length, long lastModified)
+        {
+            Preconditions.checkNotNull(file);
+            Preconditions.checkNotNull(status);
+
+            return new FileState(file, status, length, lastModified);
+        }
+
+        private final File file;
+        private final FileStatus status;
+        private final long length;
+        private final long lastModified;
+
+        protected FileState(File file, FileStatus status, long length, long lastModified)
+        {
+            this.file = file;
+            this.status = status;
+            this.length = length;
+            this.lastModified = lastModified;
+        }
+
+        public File getFile()
+        {
+            return file;
+        }
+
+        public FileStatus getStatus()
+        {
+            return status;
+        }
+
+        public long getLength()
+        {
+            return length;
+        }
+
+        public long getLastModified()
+        {
+            return lastModified;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+
+            result = prime * result + file.hashCode();
+            result = prime * result + (int) (length ^ (length >>> 32));
+            result = prime * result + (int) (lastModified ^ (lastModified >>> 32));
+            result = prime * result + status.hashCode();
+
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object other)
+        {
+            if (this == other)
+                return true;
+            if (other instanceof FileState)
+            {
+                FileState that = (FileState) other;
+                if (!this.file.equals(that.file))
+                    return false;
+                if (this.status != that.status)
+                    return false;
+                if (this.length != that.length)
+                    return false;
+                if (this.lastModified != that.lastModified)
+                    return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "FileState [" + (file != null ? "file=" + file + ", " : "") + //
+                    "status=" + status + ", length=" + length + ", lastModified=" + lastModified + "]";
+        }
+
+    }
+
+    public static FileInfo of(File file)
+    {
+        return new FileInfo(file);
+    }
+
+    private final PropertyChangeSupport pcs;
+    private volatile FileState state;
+
+    protected FileInfo(File file)
+    {
+        this.pcs = new PropertyChangeSupport(this);
+        this.state = FileState.of(file);
+    }
+
+    public FileState getState()
+    {
+        return state;
+    }
+
+    public void update(FileStatus status, long length, long lastModified)
+    {
+        Preconditions.checkNotNull(status);
+
+        state = FileState.of(state.getFile(), status, length, lastModified);
+
+        pcs.firePropertyChange(STATE_PROPERTY, null, state);
+    }
+
+    public void update()
+    {
+        File file = state.getFile();
+
+        FileStatus status;
+        long length;
+        long lastModified;
+
+        synchronized (this.state)
+        {
+            status = this.state.getStatus();
+            length = this.state.getLength();
+            lastModified = this.state.getLastModified();
+        }
+
+        status = file.isFile() ? FileStatus.ACCESSIBLE : FileStatus.INACCESSIBLE;
+        length = file.length();
+        lastModified = file.lastModified();
+
+        update(status, length, lastModified);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener)
+    {
+        pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener)
+    {
+        pcs.removePropertyChangeListener(listener);
+    }
+
+    @Override
+    public String toString()
+    {
+        return state.toString();
+    }
+
+}
