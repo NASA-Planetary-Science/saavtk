854a23bf8c17aef4222e795dcbdc8543f1236e39
James Peachey <James.Peachey@jhuapl.edu>
2019-05-13 15:25:17 -0400

    Add new file manager. This will eventually replace FileCache.
    Combines UrlInfo/UrlAccessManager with FileInfo/FileAccessManager
    to provide a facility that manages both URL and File objects.

---
+++ src/edu/jhuapl/saavtk/util/DownloadableFileInfo.java
@@ -0,0 +1,212 @@
+package edu.jhuapl.saavtk.util;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.File;
+import java.net.URL;
+
+import com.google.common.base.Preconditions;
+
+import edu.jhuapl.saavtk.util.FileInfo.FileState;
+import edu.jhuapl.saavtk.util.FileInfo.FileStatus;
+import edu.jhuapl.saavtk.util.UrlInfo.UrlState;
+import edu.jhuapl.saavtk.util.UrlInfo.UrlStatus;
+
+public class DownloadableFileInfo
+{
+    public static final String STATE_PROPERTY = "downloadableFileState";
+
+    public static class DownloadableFileState
+    {
+        public static DownloadableFileState of(URL url, File file)
+        {
+            return new DownloadableFileState(UrlState.of(url), FileState.of(file));
+        }
+
+        public static DownloadableFileState of(UrlState urlState, FileState fileState)
+        {
+            Preconditions.checkNotNull(urlState);
+            Preconditions.checkNotNull(fileState);
+
+            return new DownloadableFileState(urlState, fileState);
+        }
+
+        private final UrlState urlState;
+        private final FileState fileState;
+
+        protected DownloadableFileState(UrlState urlState, FileState fileState)
+        {
+            this.urlState = urlState;
+            this.fileState = fileState;
+        }
+
+        public UrlState getUrlState()
+        {
+            return urlState;
+        }
+
+        public FileState getFileState()
+        {
+            return fileState;
+        }
+
+        public boolean isAccessible()
+        {
+            return isLocalFileAvailable() || isUrlAccessible();
+        }
+
+        public boolean isLocalFileAvailable()
+        {
+            return fileState.getStatus() == FileStatus.ACCESSIBLE;
+        }
+
+        public boolean isUrlAccessible()
+        {
+            return urlState.getStatus() == UrlStatus.ACCESSIBLE;
+        }
+
+        public boolean isUrlUnauthorized()
+        {
+            return urlState.getStatus() == UrlStatus.NOT_AUTHORIZED;
+        }
+
+        public boolean isDownloadMayBePossible()
+        {
+            UrlStatus urlStatus = urlState.getStatus();
+
+            return urlStatus != UrlStatus.NOT_AUTHORIZED && urlStatus != UrlStatus.NOT_FOUND;
+        }
+
+        public boolean isDownloadNecessary()
+        {
+            UrlStatus urlStatus = urlState.getStatus();
+
+            boolean result = false;
+
+            switch (urlStatus)
+            {
+            case ACCESSIBLE:
+                result = fileState.getStatus() != FileStatus.ACCESSIBLE || urlState.getLastModified() > fileState.getLastModified();
+                break;
+            case NOT_AUTHORIZED:
+            case NOT_FOUND:
+                result = false;
+                break;
+            case UNKNOWN:
+                result = true;
+                break;
+            case HTTP_ERROR:
+            default:
+                result = fileState.getStatus() != FileStatus.ACCESSIBLE;
+                break;
+            }
+
+            return result;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+
+            result = prime * result + fileState.hashCode();
+            result = prime * result + urlState.hashCode();
+
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object other)
+        {
+            if (this == other)
+                return true;
+
+            if (other instanceof DownloadableFileState)
+            {
+                DownloadableFileState that = (DownloadableFileState) other;
+                if (!fileState.equals(that.fileState))
+                    return false;
+                if (!urlState.equals(that.urlState))
+                    return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "DownloadableFileState [urlState=" + urlState + ", fileState=" + fileState + "]";
+        }
+
+    }
+
+    public static DownloadableFileInfo of(URL url, File file)
+    {
+        DownloadableFileInfo result = new DownloadableFileInfo(url, file);
+
+        return result;
+    }
+
+    private final PropertyChangeSupport pcs;
+    private volatile DownloadableFileState state;
+
+    protected DownloadableFileInfo(URL url, File file)
+    {
+        this.pcs = new PropertyChangeSupport(this);
+        this.state = DownloadableFileState.of(url, file);
+    }
+
+    public DownloadableFileState getState()
+    {
+        return state;
+    }
+
+    public void update(UrlState urlState)
+    {
+        Preconditions.checkNotNull(urlState);
+
+        state = DownloadableFileState.of(urlState, state.getFileState());
+
+        pcs.firePropertyChange(STATE_PROPERTY, null, state);
+    }
+
+    public void update(FileState fileState)
+    {
+        Preconditions.checkNotNull(fileState);
+
+        state = DownloadableFileState.of(state.getUrlState(), fileState);
+
+        pcs.firePropertyChange(STATE_PROPERTY, null, state);
+    }
+
+    public void update(UrlState urlState, FileState fileState)
+    {
+        Preconditions.checkNotNull(urlState);
+        Preconditions.checkNotNull(fileState);
+
+        state = DownloadableFileState.of(urlState, fileState);
+
+        pcs.firePropertyChange(STATE_PROPERTY, null, state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener)
+    {
+        pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener)
+    {
+        pcs.removePropertyChangeListener(listener);
+    }
+
+    @Override
+    public String toString()
+    {
+        return state.toString();
+    }
+
+}
---
+++ src/edu/jhuapl/saavtk/util/DownloadableFileManager.java
@@ -0,0 +1,217 @@
+package edu.jhuapl.saavtk.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.google.common.base.Preconditions;
+
+import edu.jhuapl.saavtk.util.DownloadableFileInfo.DownloadableFileState;
+import edu.jhuapl.saavtk.util.FileInfo.FileState;
+import edu.jhuapl.saavtk.util.UrlInfo.UrlState;
+
+public class DownloadableFileManager
+{
+    public interface StateListener
+    {
+        void respond(DownloadableFileState fileState);
+    }
+
+    public static DownloadableFileManager of(UrlAccessManager urlManager, FileAccessManager fileManager)
+    {
+        return new DownloadableFileManager(urlManager, fileManager);
+    }
+
+    private final UrlAccessManager urlManager;
+    private final FileAccessManager fileManager;
+    private final ConcurrentMap<URL, DownloadableFileInfo> downloadInfoCache;
+    private final ExecutorService accessMonitor;
+    private volatile Boolean enableMonitor;
+
+    protected DownloadableFileManager(UrlAccessManager urlManager, FileAccessManager fileManager)
+    {
+        this.urlManager = urlManager;
+        this.fileManager = fileManager;
+        this.downloadInfoCache = new ConcurrentHashMap<>();
+        this.accessMonitor = Executors.newSingleThreadExecutor();
+        this.enableMonitor = Boolean.FALSE;
+    }
+
+    public synchronized void startAccessMonitor()
+    {
+        if (!enableMonitor)
+        {
+            enableMonitor = true;
+
+            accessMonitor.execute(() -> {
+                while (enableMonitor)
+                {
+                    try
+                    {
+                        for (URL url : downloadInfoCache.keySet())
+                        {
+                            query(url.toString(), false);
+                        }
+                        Thread.sleep(5000);
+                    }
+                    catch (@SuppressWarnings("unused") InterruptedException ignored)
+                    {
+
+                    }
+                    finally
+                    {
+
+                    }
+                }
+
+            });
+        }
+    }
+
+    public synchronized void stopAccessMonitor()
+    {
+        enableMonitor = false;
+    }
+
+    public boolean isAccessible(String urlString)
+    {
+        return query(urlString, false).getState().isAccessible();
+    }
+
+    public DownloadableFileInfo getInfo(String urlString)
+    {
+        Preconditions.checkNotNull(urlString);
+
+        URL url = urlManager.getUrl(urlString);
+
+        DownloadableFileInfo result = downloadInfoCache.get(url);
+        if (result == null)
+        {
+            UrlInfo urlInfo = urlManager.getInfo(url);
+            Path downloadPath = urlManager.getDownloadPath(url);
+            FileInfo fileInfo = fileManager.getInfo(downloadPath.toString());
+
+            File file = fileInfo.getState().getFile();
+
+            final DownloadableFileInfo downloadableInfo = DownloadableFileInfo.of(url, file);
+
+            urlInfo.addPropertyChangeListener(e -> {
+                if (e.getPropertyName().equals(DownloadableFileInfo.STATE_PROPERTY))
+                {
+                    downloadableInfo.update((UrlState) e.getNewValue());
+                }
+            });
+
+            fileInfo.addPropertyChangeListener(e -> {
+                if (e.getPropertyName().equals(DownloadableFileInfo.STATE_PROPERTY))
+                {
+                    downloadableInfo.update((FileState) e.getNewValue());
+                }
+            });
+
+            result = downloadableInfo;
+
+            downloadInfoCache.put(url, result);
+        }
+
+        return result;
+    }
+
+    public DownloadableFileInfo query(String urlString, boolean forceUpdate)
+    {
+        Preconditions.checkNotNull(urlString);
+
+        DownloadableFileInfo result = getInfo(urlString);
+
+        UrlInfo urlInfo = urlManager.queryServer(urlString, forceUpdate);
+
+        File file = result.getState().getFileState().getFile();
+        FileInfo fileInfo = fileManager.queryFileSystem(file, forceUpdate);
+
+        result.update(urlInfo.getState(), fileInfo.getState());
+
+        return result;
+    }
+
+    public FileDownloader getDownloader(String urlString, boolean forceDownload)
+    {
+        Preconditions.checkNotNull(urlString);
+
+        URL url = urlManager.getUrl(urlString);
+        UrlInfo urlInfo = urlManager.getInfo(urlString);
+        Path downloadPath = urlManager.getDownloadPath(url);
+
+        FileInfo fileInfo = fileManager.getInfo(downloadPath);
+
+        return FileDownloader.of(urlInfo, fileInfo, forceDownload);
+    }
+
+    public DownloadableFileState getDownloadedFile(String urlString, boolean forceDownload) throws IOException, InterruptedException
+    {
+        DownloadableFileState fileState = getInfo(urlString).getState();
+
+        if (UrlAccessManager.isServerAccessEnabled() && (forceDownload || (fileState.isDownloadMayBePossible() && fileState.isDownloadNecessary())))
+        {
+            FileDownloader downloader = getDownloader(urlString, forceDownload);
+            downloader.download();
+            fileState = getInfo(urlString).getState();
+        }
+
+        return fileState;
+    }
+
+    public void getDownloadedFile(String urlString, StateListener whenFinished, boolean forceDownload)
+    {
+        DownloadableFileState fileState = getInfo(urlString).getState();
+
+        if (UrlAccessManager.isServerAccessEnabled() && (forceDownload || (fileState.isDownloadMayBePossible() && fileState.isDownloadNecessary())))
+        {
+            FileDownloader downloader = getDownloader(urlString, forceDownload);
+
+            downloader.addPropertyChangeListener(e -> {
+                String propertyName = e.getPropertyName();
+                if (propertyName.equals(FileDownloader.DONE_PROPERTY) || propertyName.equals(FileDownloader.CANCELED_PROPERTY))
+                {
+                    whenFinished.respond(getInfo(urlString).getState());
+                }
+            });
+
+            downloader.downloadInBackground();
+        }
+        else
+        {
+            whenFinished.respond(fileState);
+        }
+    }
+
+    public void addStateListener(String urlString, StateListener listener)
+    {
+        Preconditions.checkNotNull(listener);
+
+        getInfo(urlString).addPropertyChangeListener(e -> {
+            if (e.getPropertyName().equals(DownloadableFileInfo.STATE_PROPERTY))
+            {
+                listener.respond((DownloadableFileState) e.getNewValue());
+            }
+        });
+    }
+
+    public void removeStateListener(String urlString, StateListener listener)
+    {
+        Preconditions.checkNotNull(listener);
+
+        // TODO: this is obviously wrong; need a better way to manage this.
+        getInfo(urlString).removePropertyChangeListener(e -> {
+            if (e.getPropertyName().equals(DownloadableFileInfo.STATE_PROPERTY))
+            {
+                listener.respond((DownloadableFileState) e.getNewValue());
+            }
+        });
+    }
+
+}
