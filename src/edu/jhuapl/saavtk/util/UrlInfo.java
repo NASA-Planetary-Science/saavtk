4e7f015f54313442213d5bb87dfe4a24451c3a4d
James Peachey <James.Peachey@jhuapl.edu>
2019-05-13 09:00:34 -0400

    Decoupled URL information from file information.

--- src/edu/jhuapl/saavtk/util/UrlInfo.java
+++ src/edu/jhuapl/saavtk/util/UrlInfo.java
@@ -1,74 +1,141 @@
 package edu.jhuapl.saavtk.util;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Path;
 
 import com.google.common.base.Preconditions;
 
+/**
+ * Collect and store essential properties associated with a remote resource.
+ * These properties are useful for determining whether it is necessary and/or
+ * possible to download a remote file, or to determine the size of the file.
+ */
 public class UrlInfo
 {
     public enum UrlStatus
     {
         ACCESSIBLE, // Connection made, resource is accessible. Future access likely to succeed.
         NOT_AUTHORIZED, // Connection made, but user is not authorized.
+        NOT_FOUND, // Connection made, but resource was not found.
         HTTP_ERROR, // Connection made, but HTTP error code was returned. Future access unknown.
         UNKNOWN, // Have not succesfully obtained information about the URL.
     }
 
-    public static UrlInfo of(URL url, Path relativePath)
+    public static class UrlState
     {
-        Preconditions.checkNotNull(url);
-        Preconditions.checkNotNull(relativePath);
+        public static UrlState of()
+        {
+            return new UrlState(null, UrlStatus.UNKNOWN, -1, 0);
+        }
 
-        return new UrlInfo(url, relativePath);
+        private final URL url;
+        private final UrlStatus status;
+        private final long contentLength;
+        private final long lastModified;
+
+        public UrlState(URL url, UrlStatus status, long contentLength, long lastModified)
+        {
+            this.url = url;
+            this.status = status;
+            this.contentLength = contentLength;
+            this.lastModified = lastModified;
+        }
+
+        public URL getUrl()
+        {
+            return url;
+        }
+
+        public UrlStatus getStatus()
+        {
+            return status;
+        }
+
+        public long getContentLength()
+        {
+            return contentLength;
+        }
+
+        public long getLastModified()
+        {
+            return lastModified;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + url.hashCode();
+            result = prime * result + (int) (contentLength ^ (contentLength >>> 32));
+            result = prime * result + (int) (lastModified ^ (lastModified >>> 32));
+            result = prime * result + status.hashCode();
+
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object other)
+        {
+            if (this == other)
+                return true;
+            if (other instanceof UrlState)
+            {
+                UrlState that = (UrlState) other;
+                if (!this.url.equals(that.url))
+                    return false;
+                if (this.contentLength != that.contentLength)
+                    return false;
+                if (this.lastModified != that.lastModified)
+                    return false;
+                if (this.status != that.status)
+                    return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "UrlState [" + (url != null ? "url=" + url + ", " : "") + "status=" + status + ", contentLength=" + contentLength + ", lastModified=" + lastModified + "]";
+        }
+
     }
 
-    private final URL url;
-    private final Path relativePath;
-    private volatile UrlStatus status;
-    private volatile long lastModified;
-
-    protected UrlInfo(URL url, Path relativePath)
+    public static UrlInfo of()
     {
-        this.url = url;
-        this.relativePath = relativePath;
-        this.status = UrlStatus.UNKNOWN;
-        this.lastModified = 0;
+        return new UrlInfo();
     }
 
-    public URL getUrl()
+    private final PropertyChangeSupport pcs;
+    private volatile UrlState state;
+
+    protected UrlInfo()
     {
-        return url;
+        this.pcs = new PropertyChangeSupport(this);
+        this.state = UrlState.of();
     }
 
-    public Path getRelativePath()
+    public UrlState getState()
     {
-        return relativePath;
+        return state;
     }
 
-    public UrlStatus getStatus()
-    {
-        return status;
-    }
-
-    public long getLastModified()
-    {
-        return lastModified;
-    }
-
-    public void update(UrlStatus status, long lastModified)
+    public void update(URL url, UrlStatus status, long contentLength, long lastModified)
     {
         Preconditions.checkNotNull(status);
 
-        synchronized (this.status)
-        {
-            this.status = status;
-            this.lastModified = lastModified;
-        }
+        state = new UrlState(url, status, contentLength, lastModified);
+
+        pcs.firePropertyChange("state", null, state);
     }
 
     public void update(URLConnection connection) throws IOException
@@ -76,12 +143,14 @@ public class UrlInfo
         Preconditions.checkNotNull(connection);
 
         UrlStatus status;
+        long contentLength;
         long lastModified;
 
-        synchronized (this.status)
+        synchronized (this.state)
         {
-            status = this.status;
-            lastModified = this.lastModified;
+            status = this.state.getStatus();
+            contentLength = this.state.getContentLength();
+            lastModified = this.state.getLastModified();
         }
 
         if (connection instanceof HttpURLConnection)
@@ -96,15 +165,20 @@ public class UrlInfo
                 if (code >= HttpURLConnection.HTTP_OK && code < HttpURLConnection.HTTP_MULT_CHOICE)
                 {
                     status = UrlStatus.ACCESSIBLE;
+                    contentLength = httpConnection.getContentLengthLong();
                     lastModified = httpConnection.getLastModified();
                 }
                 else if (code == HttpURLConnection.HTTP_UNAUTHORIZED || code == HttpURLConnection.HTTP_FORBIDDEN)
                 {
                     status = UrlStatus.NOT_AUTHORIZED;
                 }
+                else if (code == HttpURLConnection.HTTP_NOT_FOUND)
+                {
+                    status = UrlStatus.NOT_FOUND;
+                }
                 else
                 {
-                    Debug.err().println("Received response code " + code + " for URL " + getUrl());
+                    Debug.err().println("Received response code " + code + " for URL " + connection.getURL());
                     status = UrlStatus.HTTP_ERROR;
                 }
             }
@@ -121,52 +195,23 @@ public class UrlInfo
             status = UrlStatus.ACCESSIBLE;
         }
 
-        update(status, lastModified);
+        update(connection.getURL(), status, contentLength, lastModified);
     }
 
-    @Override
-    public int hashCode()
+    public void addPropertyChangeListener(PropertyChangeListener listener)
     {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + url.hashCode();
-        result = prime * result + relativePath.hashCode();
-
-        return result;
+        pcs.addPropertyChangeListener(listener);
     }
 
-    @Override
-    public boolean equals(Object other)
+    public void removePropertyChangeListener(PropertyChangeListener listener)
     {
-        if (this == other)
-        {
-            return true;
-        }
-        if (other instanceof UrlInfo)
-        {
-            UrlInfo that = (UrlInfo) other;
-            if (!this.url.equals(that.url))
-            {
-                return false;
-            }
-            if (!this.relativePath.equals(that.relativePath))
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        return false;
+        pcs.removePropertyChangeListener(listener);
     }
 
     @Override
     public String toString()
     {
-        synchronized (this.status)
-        {
-            return url.toString() + " (status=" + status + ", lastModified=" + lastModified + ")";
-        }
+        return state.toString();
     }
 
 }
