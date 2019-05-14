1a7fca8663883d6453a3276b8211296b61e55359
James Peachey <James.Peachey@jhuapl.edu>
2019-05-14 08:19:30 -0400

    Auto-format.

--- src/edu/jhuapl/saavtk/util/Debug.java
+++ src/edu/jhuapl/saavtk/util/Debug.java
@@ -6,31 +6,31 @@ import org.apache.commons.io.output.NullOutputStream;
 
 public class Debug
 {
-	private static final PrintStream NULL_STREAM = new PrintStream(new NullOutputStream());
-	private static boolean enabled = false;
+    private static final PrintStream NULL_STREAM = new PrintStream(new NullOutputStream());
+    private static boolean enabled = false;
 
-	public static boolean isEnabled()
-	{
-		return enabled;
-	}
+    public static boolean isEnabled()
+    {
+        return enabled;
+    }
 
-	public static void setEnabled(boolean enabled)
-	{
-		Debug.enabled = enabled;
-	}
+    public static void setEnabled(boolean enabled)
+    {
+        Debug.enabled = enabled;
+    }
 
-	public static PrintStream err()
-	{
-		return Debug.enabled ? System.err : NULL_STREAM;
-	}
+    public static PrintStream err()
+    {
+        return Debug.enabled ? System.err : NULL_STREAM;
+    }
 
-	public static PrintStream out()
-	{
-		return Debug.enabled ? System.out : NULL_STREAM;
-	}
+    public static PrintStream out()
+    {
+        return Debug.enabled ? System.out : NULL_STREAM;
+    }
 
-	private Debug()
-	{
-		throw new AssertionError();
-	}
+    private Debug()
+    {
+        throw new AssertionError();
+    }
 }
