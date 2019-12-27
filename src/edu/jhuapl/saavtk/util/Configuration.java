40e05c3181f5dcab785c9df8ef5d4264a82d5d76
James Peachey <James.Peachey@jhuapl.edu>
2019-12-27 13:12:27 -0500

    RM 2045: this is really the "right way" to check for headless mode.

--- src/edu/jhuapl/saavtk/util/Configuration.java
+++ src/edu/jhuapl/saavtk/util/Configuration.java
@@ -1,6 +1,7 @@
 package edu.jhuapl.saavtk.util;
 
 import java.awt.EventQueue;
+import java.awt.GraphicsEnvironment;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -81,7 +82,7 @@ public class Configuration
     {
         if (headless == null)
         {
-            headless = Boolean.parseBoolean(System.getProperty("java.awt.headless"));
+            headless = GraphicsEnvironment.isHeadless();
         }
 
         return headless;
