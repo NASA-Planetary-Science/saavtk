bfec876e93a7b32b68125250c824e848bdfc8654
James Peachey <James.Peachey@jhuapl.edu>
2019-12-27 13:20:33 -0500

    RM 2045: auto-format.

--- src/edu/jhuapl/saavtk/util/ConvertObjToVtk.java
+++ src/edu/jhuapl/saavtk/util/ConvertObjToVtk.java
@@ -8,20 +8,23 @@ import vtk.vtkPolyDataWriter;
 
 public class ConvertObjToVtk
 {
-    static { vtkNativeLibrary.LoadAllNativeLibraries(); }
+    static
+    {
+        vtkNativeLibrary.LoadAllNativeLibraries();
+    }
 
     public static void main(String[] args)
     {
-        String objFile=args[0];
-        System.out.print("Reading "+objFile+"... ");
+        String objFile = args[0];
+        System.out.print("Reading " + objFile + "... ");
 
-        vtkOBJReader reader=new vtkOBJReader();
+        vtkOBJReader reader = new vtkOBJReader();
         reader.SetFileName(objFile);
         reader.Update();
 
-        String vtkFile=FilenameUtils.removeExtension(objFile)+".vtk";
-        System.out.print("Writing to "+vtkFile+"... ");
-        vtkPolyDataWriter writer=new vtkPolyDataWriter();
+        String vtkFile = FilenameUtils.removeExtension(objFile) + ".vtk";
+        System.out.print("Writing to " + vtkFile + "... ");
+        vtkPolyDataWriter writer = new vtkPolyDataWriter();
         writer.SetFileName(vtkFile);
         writer.SetFileTypeToBinary();
         writer.SetInputData(reader.GetOutput());
