f8d9c036189f5882b982d4124317d20e7df9a338
James Peachey <James.Peachey@jhuapl.edu>
2019-12-30 10:39:31 -0500

    RM 2045: call loadAllVtkLibraries; this one doesn't work headless.

--- src/edu/jhuapl/saavtk/gui/jogl/StereoRenderCanvas.java
+++ src/edu/jhuapl/saavtk/gui/jogl/StereoRenderCanvas.java
@@ -89,7 +89,7 @@ public class StereoRenderCanvas extends vtksbmtJoglCanvas
 
     public static void main(String[] args)
     {
-        NativeLibraryLoader.loadVtkLibraries();
+        NativeLibraryLoader.loadAllVtkLibraries();
 
         vtkConeSource source = new vtkConeSource();
         source.Update();
