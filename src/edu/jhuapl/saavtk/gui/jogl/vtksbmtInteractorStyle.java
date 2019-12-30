d065d044b3a02ea28667d423b2b796bf8c1a5166
James Peachey <James.Peachey@jhuapl.edu>
2019-12-30 10:46:33 -0500

    RM 2045: call loadAllVtkLibraries. This one doesn't run headless.

--- src/edu/jhuapl/saavtk/gui/jogl/vtksbmtInteractorStyle.java
+++ src/edu/jhuapl/saavtk/gui/jogl/vtksbmtInteractorStyle.java
@@ -639,7 +639,7 @@ class vtksbmtInteractorStyle extends vtkInteractorStyle implements MouseListener
 
     public static void main(String[] args)
     {
-        NativeLibraryLoader.loadVtkLibraries();
+        NativeLibraryLoader.loadAllVtkLibraries();
         vtksbmtJoglCanvas canvas = new vtksbmtJoglCanvas();
         canvas.setInteractorStyle(new vtksbmtInteractorStyle(canvas));
 
