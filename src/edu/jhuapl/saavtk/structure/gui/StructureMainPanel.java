688f4573814754892b74452466b08ec0702d1b87
lopeznr1 <lopeznr1@jhuapl.edu>
2020-06-05 15:23:23 -0400

    Enhanced structure loading capabilities
    [1] Created UI panel AmbiguousPanel
    [2] Logic to handle ambiguous round structures (circles, points)
    [3] Logic to handle structures generated with releases post 2020Apr

--- src/edu/jhuapl/saavtk/structure/gui/StructureMainPanel.java
+++ src/edu/jhuapl/saavtk/structure/gui/StructureMainPanel.java
@@ -18,16 +18,22 @@ import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+
 import edu.jhuapl.saavtk.gui.StatusBar;
 import edu.jhuapl.saavtk.gui.dialog.CustomFileChooser;
 import edu.jhuapl.saavtk.gui.funk.PopupButton;
 import edu.jhuapl.saavtk.gui.render.Renderer;
 import edu.jhuapl.saavtk.model.ModelManager;
 import edu.jhuapl.saavtk.model.ModelNames;
+import edu.jhuapl.saavtk.model.structure.AbstractEllipsePolygonModel.Mode;
 import edu.jhuapl.saavtk.pick.PickManager;
+import edu.jhuapl.saavtk.structure.Ellipse;
 import edu.jhuapl.saavtk.structure.Structure;
 import edu.jhuapl.saavtk.structure.StructureManager;
 import edu.jhuapl.saavtk.structure.gui.action.LoadEsriShapeFileAction;
+import edu.jhuapl.saavtk.structure.gui.load.AmbiguousRoundStructurePanel;
 import edu.jhuapl.saavtk.structure.gui.load.LoadPanel;
 import edu.jhuapl.saavtk.structure.io.StructureLoadUtil;
 import glum.gui.GuiUtil;
@@ -103,9 +109,9 @@ public class StructureMainPanel extends JPanel implements ActionListener
 		List<Structure> fullL = new ArrayList<>();
 		for (File aFile : tmpFileL)
 		{
-			List<Structure> tmpL;
 			try
 			{
+				List<Structure> tmpL;
 				tmpL = StructureLoadUtil.loadStructures(aFile);
 				fullL.addAll(tmpL);
 			}
@@ -115,6 +121,11 @@ public class StructureMainPanel extends JPanel implements ActionListener
 				failM.put(aFile, aExp);
 			}
 		}
+
+		// Check for ambiguous structures and transform no non-ambiguous structures
+		fullL = handleAmbiguousStructuresOrAbort(fullL);
+		if (fullL == null)
+			return;
 
 		// Lazy init
 		if (loadDialog == null)
@@ -193,6 +204,62 @@ public class StructureMainPanel extends JPanel implements ActionListener
 		}
 
 		return retL;
+	}
+
+	/**
+	 * Helper method that will determine if there are ambiguous and if so prompt the
+	 * user for how to handle them. If abort is canceled then null will be returned
+	 * and the load process should be aborted.
+	 */
+	private List<Structure> handleAmbiguousStructuresOrAbort(List<Structure> aItemL)
+	{
+		// Determine the ambiguous structures (source file to structure)
+		Multimap<Object, Structure> ambigouseMM = LinkedListMultimap.create();
+		for (Structure aStructure : aItemL)
+		{
+			if (aStructure instanceof Ellipse && ((Ellipse) aStructure).getMode() == null)
+				ambigouseMM.put(aStructure.getSource(), aStructure);
+		}
+
+		// Bail since there is no ambiguous structures
+		if (ambigouseMM.size() == 0)
+			return aItemL;
+
+		// Prompt the user for how to handle the ambiguous structures.
+		AmbiguousRoundStructurePanel tmpPrompPanel = new AmbiguousRoundStructurePanel(this, 550, 225);
+		tmpPrompPanel.setAmbigousMap(ambigouseMM);
+		tmpPrompPanel.setVisibleAsModal();
+
+		// Bail if no mode selected
+		Mode pickMode = tmpPrompPanel.getSelection();
+		if (pickMode == null)
+			return null;
+
+		// Transform the ambiguous Structures with the user's selection
+		List<Structure> retItemL = new ArrayList<>();
+		for (Structure aItem : aItemL)
+		{
+			// Skip to next if not an ellipse
+			if (aItem instanceof Ellipse == false)
+			{
+				retItemL.add(aItem);
+				continue;
+			}
+
+			// Skip to next if not an (ambiguous) ellipse
+			Ellipse tmpItem = (Ellipse) aItem;
+			if (tmpItem.getMode() != null)
+			{
+				retItemL.add(tmpItem);
+				continue;
+			}
+
+			tmpItem = new Ellipse(tmpItem.getId(), tmpItem.getSource(), pickMode, tmpItem.getCenter(), tmpItem.getRadius(),
+					tmpItem.getAngle(), tmpItem.getFlattening(), tmpItem.getColor(), tmpItem.getLabel());
+			retItemL.add(tmpItem);
+		}
+
+		return retItemL;
 	}
 
 	/**
