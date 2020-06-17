e6c2632f6c0d4426fc44645cb98c0332fed2ebbc
lopeznr1 <lopeznr1@jhuapl.edu>
2020-06-17 09:10:50 -0400

    Changes to reflect updated Glum library

--- src/edu/jhuapl/saavtk/scalebar/gui/ScaleBarAction.java
+++ src/edu/jhuapl/saavtk/scalebar/gui/ScaleBarAction.java
@@ -12,10 +12,14 @@ import javax.swing.AbstractAction;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
+import edu.jhuapl.saavtk.gui.View;
 import edu.jhuapl.saavtk.gui.ViewManager;
 import edu.jhuapl.saavtk.gui.render.Renderer;
+import edu.jhuapl.saavtk.gui.render.SceneChangeNotifier;
 import edu.jhuapl.saavtk.scalebar.ScaleBarPainter;
+import glum.gui.GuiUtil;
 import glum.gui.action.CloseDialog;
+import glum.misc.InitListener;
 
 /**
  * {@link AbstractAction} that contains the logic for the 'Config Scale Bar'
@@ -23,7 +27,7 @@ import glum.gui.action.CloseDialog;
  *
  * @author lopeznr1
  */
-public class ScaleBarAction extends AbstractAction implements HierarchyListener
+public class ScaleBarAction extends AbstractAction implements HierarchyListener, InitListener
 {
 	// Constants
 	private static final String Title = "Scale Bar";
@@ -32,7 +36,7 @@ public class ScaleBarAction extends AbstractAction implements HierarchyListener
 	private final ViewManager refViewManager;
 
 	// State vars
-	private final Map<Renderer, JDialog> viewM;
+	private final Map<View, JDialog> initM;
 
 	/**
 	 * Standard Constructor
@@ -43,38 +47,65 @@ public class ScaleBarAction extends AbstractAction implements HierarchyListener
 
 		refViewManager = aViewManager;
 
-		viewM = new HashMap<>();
+		initM = new HashMap<>();
+
+		// Register for events of interest
+		refViewManager.addInitListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent aEvent)
 	{
-		Renderer tmpRenderer = refViewManager.getCurrentView().getRenderer();
+		View tmpView = refViewManager.getCurrentView();
 
-		JDialog tmpDialog = viewM.get(tmpRenderer);
+		// Locate and show the corresponding dialog
+		JDialog tmpDialog = initM.get(tmpView);
 		if (tmpDialog == null)
-		{
-			ScaleBarPainter tmpScaleBarPainter = refViewManager.getCurrentView().getScaleBarPainter();
-			ScaleBarPanel tmpPanel = new ScaleBarPanel(tmpRenderer, tmpScaleBarPainter);
+			return;
 
-			Frame tmpFrame = JOptionPane.getFrameForComponent(refViewManager.getCurrentView());
-			tmpDialog = new CloseDialog(tmpFrame, tmpPanel);
-			tmpDialog.setTitle(Title);
-			tmpDialog.setLocationRelativeTo(tmpRenderer);
-			tmpDialog.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
-
-			tmpRenderer.addHierarchyListener(this);
-
-			viewM.put(tmpRenderer, tmpDialog);
-		}
 		tmpDialog.setVisible(true);
+	}
+
+	@Override
+	public void handleInitAction(Object aSource)
+	{
+		// Ignore if the source is not a View
+		if (aSource instanceof View == false)
+			return;
+
+		// Execute on the AWT
+		if (GuiUtil.redispatchOnAwtIfNeeded(() -> handleInitAction(aSource)) == true)
+			return;
+
+		View tmpView = (View) aSource;
+		Renderer tmpRenderer = tmpView.getRenderer();
+		SceneChangeNotifier tmpSceneChangeNotifier = tmpView.getModelManager();
+
+		// Bail if we have already seen this source
+		JDialog tmpDialog = initM.get(tmpView);
+		if (tmpDialog != null)
+			return;
+
+		// Create our UI
+		ScaleBarPainter tmpPainter = new ScaleBarPainter(tmpRenderer, tmpSceneChangeNotifier);
+		tmpRenderer.addVtkPropProvider(tmpPainter);
+		ScaleBarPanel tmpPanel = new ScaleBarPanel(tmpRenderer, tmpPainter);
+
+		Frame tmpFrame = JOptionPane.getFrameForComponent(tmpView);
+		tmpDialog = new CloseDialog(tmpFrame, tmpPanel);
+		tmpDialog.setTitle(Title);
+		tmpDialog.setLocationRelativeTo(tmpRenderer);
+		initM.put(tmpView, tmpDialog);
+
+		// Register for events of interest
+		tmpRenderer.addHierarchyListener(this);
 	}
 
 	@Override
 	public void hierarchyChanged(HierarchyEvent aEvent)
 	{
 		Component tmpComp = aEvent.getComponent();
-		JDialog tmpDialog = viewM.get(tmpComp);
+		JDialog tmpDialog = initM.get(tmpComp);
 		if (tmpDialog != null && tmpDialog.isShowing() == true && tmpComp.isShowing() == false)
 			tmpDialog.setVisible(false);
 	}
