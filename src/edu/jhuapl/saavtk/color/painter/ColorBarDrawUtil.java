925f5126d84439aa28f5869edeec5b9c9e482ff0
lopeznr1 <lopeznr1@jhuapl.edu>
2020-10-01 15:16:20 -0400

    Various enhancements to color bar related code
    [1] Cleaned up UI of primary color bar panel
    [1.1] Redesigned color bar panel for more dense and informative UI
    [1.2] Added individual min, max reset buttons
    [1.3] Added support for dynamic tool tips for min, max reset buttons
    [1.4] Added support to reset location
    
    [2] Introduced axis mode concept: Cadence, Logarithmic, Standard
    [2.1] Cadence mode supports mark / beat
    [2.2] Updated Logarithmic mode as first class control
    [2.3] Added checking for error / warning conditions
    
    [3] Added support for fine grained color bar configuration
    [3.1] Support for changing entire color bar visibility
    [3.2] Added 'Layout' configuration panel
    [3.2.1] Manual orientation control
    [3.2.2] Support for reverse color / display
    [3.2.3] Support for configuration of axis mode
    [3.2.4] Support for manual bar length, bar width
    [3.3] Added font configuration for title, labels
    [3.3.1] Support for changing title / label visibility
    [3.3.2] Support for changing font face, bold, italic
    [3.3.3] Support for changing font size + color
    [3.4] Added 'Background' configuration panel
    [3.4.1] Added visibility mode: Always, Never, Auto,
    [3.4.2] Support for changing background color (RGBA)
    
    [4] Code cleanup.

---
+++ src/edu/jhuapl/saavtk/color/painter/ColorBarDrawUtil.java
@@ -0,0 +1,244 @@
+package edu.jhuapl.saavtk.color.painter;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import edu.jhuapl.saavtk.color.gui.bar.LayoutAttr;
+import edu.jhuapl.saavtk.color.table.ColorMapAttr;
+import edu.jhuapl.saavtk.vtk.font.FontAttr;
+import glum.text.SigFigNumberFormat;
+import plotkit.AxisTransform;
+import plotkit.cadence.Cadence;
+import plotkit.cadence.ModelLogIterator;
+import plotkit.cadence.PlainModelCadence;
+import plotkit.tranform.LogAxisTransform;
+import plotkit.tranform.PlainAxisTransform;
+import vtk.vtkTextActor;
+
+/**
+ * Collection of utility methods associated with drawing different components of
+ * a {@link ColorBarPainter}. The draw routines will draw the components to a
+ * "normalized" coordinate system. The normalized coordinate system will
+ * <B>assume</B> the color bar is located at the origin (0, 0).
+ *
+ * @author lopeznr1
+ */
+public class ColorBarDrawUtil
+{
+	/**
+	 * Utility method that returns the (approximate) number of labels needed to
+	 * render all the labels with the specified configuration.
+	 */
+	public static int computeNumLabelsNeededFor(LayoutAttr aTmpLA, ColorMapAttr aTmpCMA)
+	{
+		Cadence tmpCadence = aTmpLA.getCadence();
+
+		// If the cadence will be auto generated then just use the number of ticks
+		boolean isAutoCadence = false;
+		isAutoCadence |= aTmpCMA.getIsLogScale() == true;
+		isAutoCadence |= aTmpLA.getIsCadenceEnabled() == false;
+		isAutoCadence |= tmpCadence == Cadence.Invalid;
+		if (isAutoCadence == true)
+			return aTmpLA.getNumTicks();
+
+		// Retrieve the range of data values
+		double minDV = aTmpCMA.getMinVal();
+		double maxDV = aTmpCMA.getMaxVal();
+
+		if (tmpCadence instanceof PlainModelCadence)
+		{
+			double beat = ((PlainModelCadence) tmpCadence).getBeat();
+
+			int retVal = (int) ((maxDV - minDV) / beat) + 1;
+			return retVal;
+		}
+
+		throw new RuntimeException("Unsupported cadence type: " + tmpCadence.getClass().getName());
+	}
+
+	/**
+	 * Utility method that will draw the numeric labels along the color bar. Note
+	 * the labels will be positioned at a "normalized" position. The normalized
+	 * position will <B>assume</B> the color bar is located at the origin (0, 0).
+	 * <P>
+	 * The list of utilized (vtkTextActor) labels will be returned.
+	 *
+	 * @param aLabelL The list of available ({@link vtkTextActor}) labels
+	 * @param aTmpCMA The {@link ColorMapAttr} associated with the color bar.
+	 * @param aTmpLA  The {@link LayoutAttr} associated with the color bar
+	 */
+	public static List<vtkTextActor> drawLabels(List<vtkTextActor> aLabelL, ColorMapAttr aTmpCMA, LayoutAttr aTmpLA)
+	{
+		// Retrieve the range of data values
+		double minDV = aTmpCMA.getMinVal();
+		double maxDV = aTmpCMA.getMaxVal();
+
+		// Bail if we do not have a valid data range
+		boolean isValidDataRange = true;
+		isValidDataRange &= Double.isNaN(minDV) == false;
+		isValidDataRange &= Double.isNaN(maxDV) == false;
+		isValidDataRange &= maxDV > minDV;
+		if (isValidDataRange == false)
+			return ImmutableList.of();
+
+		double diffDV = maxDV - minDV;
+		double axisLen = aTmpLA.getBarLength();
+		double scaleFact = diffDV / axisLen;
+
+		// Retrieve the cadence
+		int numTicks = aTmpLA.getNumTicks();
+		Cadence tmpCadence = aTmpLA.getCadence();
+		if (aTmpLA.getIsCadenceEnabled() == false || tmpCadence == Cadence.Invalid)
+		{
+			// Bail if no ticks are to be drawn
+			if (numTicks == 0)
+				return ImmutableList.of();
+
+			double tmpBeat = diffDV;
+			double tmpMark = minDV + (diffDV / 2.0);
+			if (numTicks > 1)
+			{
+				tmpBeat = diffDV / (numTicks - 1);
+				tmpMark = minDV;
+			}
+			tmpCadence = new PlainModelCadence(tmpBeat, tmpMark);
+		}
+
+		// Retrieve the value Iterator and the AxisTransform
+		AxisTransform tmpAxisTransform = new PlainAxisTransform(minDV, maxDV, scaleFact, 0.0);
+		Iterator<Double> tmpIter = tmpCadence.getIter(tmpAxisTransform, 0, axisLen + 0.01);
+		if (aTmpCMA.getIsLogScale() == true)
+		{
+			tmpAxisTransform = new LogAxisTransform(minDV, maxDV, scaleFact, 0.0);
+			tmpIter = new ModelLogIterator(minDV, maxDV, numTicks);
+		}
+
+		// Delegate
+		if (aTmpLA.getIsHorizontal() == true)
+			return drawLabelsHorizontal(aLabelL, aTmpLA, tmpAxisTransform, tmpIter);
+		else
+			return drawLabelsVertical(aLabelL, aTmpLA, tmpAxisTransform, tmpIter);
+	}
+
+	/**
+	 * Utility method that will draw the title along the color bar. Note the title
+	 * will be positioned at a "normalized" position. The normalized position will
+	 * <B>assume</B> the color bar is located at the origin (0, 0).
+	 *
+	 * @param aVtkTitleTA     The {@link vtkTextActor} used to render the title.
+	 * @param aTmpLA          The {@link LayoutAttr} associated with the color bar
+	 * @param aUtilizedLabelL The list of utilized labels. These labels should be
+	 *                        positioned in a "normalized" position.
+	 */
+	public static void drawTitle(vtkTextActor aVtkTitleTA, LayoutAttr aTmpLA, FontAttr aLabelFA,
+			List<vtkTextActor> aUtilizedLabelL)
+	{
+		boolean isHorizontal = aTmpLA.getIsHorizontal();
+		double normX, normY;
+		if (isHorizontal == true)
+		{
+			normX = 0 + aTmpLA.getBarLength() / 2.0;
+			normY = 0 + aTmpLA.getBarWidth();
+			aVtkTitleTA.GetTextProperty().SetJustificationToCentered();
+			aVtkTitleTA.GetTextProperty().SetVerticalJustificationToBottom();
+		}
+		else
+		{
+			normX = 0;
+			normY = 0 + aTmpLA.getBarLength();
+
+			// Determine the label with the maximum y-position
+			double maxLabelY = 0;
+			for (vtkTextActor aTmpTA : aUtilizedLabelL)
+			{
+				double[] tmpPosArr = aTmpTA.GetPosition();
+				double tmpLabelY = tmpPosArr[1] + aLabelFA.getSize() / 2.0;
+				if (tmpLabelY > maxLabelY)
+					maxLabelY = tmpLabelY;
+			}
+
+			if (maxLabelY > normY)
+				normY = maxLabelY;
+
+			aVtkTitleTA.GetTextProperty().SetJustificationToLeft();
+			aVtkTitleTA.GetTextProperty().SetVerticalJustificationToBottom();
+		}
+		aVtkTitleTA.SetPosition(normX, normY);
+	}
+
+	/**
+	 * Helper utility method that will draw the the axis labels along the x-axis
+	 * direction.
+	 */
+	private static List<vtkTextActor> drawLabelsHorizontal(List<vtkTextActor> aLabelL, LayoutAttr aTmpLA,
+			AxisTransform tmpAxisTransform, Iterator<Double> tmpIter)
+	{
+		SigFigNumberFormat numFormat = new SigFigNumberFormat(3);
+		double axisLen = aTmpLA.getBarLength();
+
+		List<vtkTextActor> retLabelL = new ArrayList<>();
+		for (int c1 = 0; c1 < aLabelL.size(); c1++)
+		{
+			// Bail once we run out of values
+			if (tmpIter.hasNext() == false)
+				return retLabelL;
+
+			double rawVal = tmpIter.next();
+
+			double absX = tmpAxisTransform.getAxisValForPlotVal(rawVal);
+			double normY = 0.0;
+			double normX = absX;
+			if (aTmpLA.getIsReverseOrder() == true)
+				normX = axisLen - absX;
+
+			vtkTextActor vTmpTA = aLabelL.get(c1);
+			vTmpTA.SetInput(numFormat.format(rawVal));
+			vTmpTA.SetPosition(normX, normY);
+			vTmpTA.GetTextProperty().SetJustificationToCentered();
+			vTmpTA.GetTextProperty().SetVerticalJustificationToTop();
+			retLabelL.add(vTmpTA);
+		}
+
+		return retLabelL;
+	}
+
+	/**
+	 * Helper utility method that will draw the the axis labels along the y-axis
+	 * direction.
+	 */
+	private static List<vtkTextActor> drawLabelsVertical(List<vtkTextActor> aLabelL, LayoutAttr aTmpLA,
+			AxisTransform tmpAxisTransform, Iterator<Double> tmpIter)
+	{
+		SigFigNumberFormat numFormat = new SigFigNumberFormat(3);
+		double axisLen = aTmpLA.getBarLength();
+
+		List<vtkTextActor> retLabelL = new ArrayList<>();
+		for (int c1 = 0; c1 < aLabelL.size(); c1++)
+		{
+			// Bail once we run out of values
+			if (tmpIter.hasNext() == false)
+				return retLabelL;
+
+			double rawVal = tmpIter.next();
+
+			double absY = tmpAxisTransform.getAxisValForPlotVal(rawVal);
+			double normX = aTmpLA.getBarWidth();
+			double normY = absY;
+			if (aTmpLA.getIsReverseOrder() == true)
+				normY = axisLen - absY;
+
+			vtkTextActor vTmpTA = aLabelL.get(c1);
+			vTmpTA.SetInput(numFormat.format(rawVal));
+			vTmpTA.SetPosition(normX, normY);
+			vTmpTA.GetTextProperty().SetJustificationToLeft();
+			vTmpTA.GetTextProperty().SetVerticalJustificationToCentered();
+			retLabelL.add(vTmpTA);
+		}
+
+		return retLabelL;
+	}
+
+}
--- src/edu/jhuapl/saavtk/scalebar/ScaleBarPainter.java
+++ src/edu/jhuapl/saavtk/scalebar/ScaleBarPainter.java
@@ -12,13 +12,9 @@ import edu.jhuapl.saavtk.gui.render.Renderer;
 import edu.jhuapl.saavtk.gui.render.VtkPropProvider;
 import edu.jhuapl.saavtk.view.ViewActionListener;
 import edu.jhuapl.saavtk.view.ViewChangeReason;
+import edu.jhuapl.saavtk.vtk.Location;
 import edu.jhuapl.saavtk.vtk.VtkResource;
-import vtk.vtkActor2D;
-import vtk.vtkCellArray;
-import vtk.vtkIdList;
-import vtk.vtkPoints;
-import vtk.vtkPolyData;
-import vtk.vtkPolyDataMapper2D;
+import edu.jhuapl.saavtk.vtk.painter.VtkRectPainter;
 import vtk.vtkProp;
 import vtk.vtkTextActor;
 import vtk.rendering.jogl.vtkJoglPanelComponent;
@@ -38,51 +34,26 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 
 	// State vars
 	private boolean isVisible;
-	private double dimSizeX, dimSizeY;
+	private double dimX, dimY;
 	private NumberFormat dispNF;
 
 	// VTK vars
-	private vtkPolyData vScaleBarPD;
-	private vtkPolyDataMapper2D vScaleBarM;
-	private vtkActor2D vScaleBarA;
+	private VtkRectPainter vScaleBarRP;
 	private vtkTextActor vScaleBarTA;
 
-	/**
-	 * Standard Constructor
-	 */
+	/** Standard Constructor */
 	public ScaleBarPainter(Renderer aRenderer)
 	{
 		refRenderer = aRenderer;
 
 		isVisible = true;
-		dimSizeX = 150;
-		dimSizeY = 16;
+		dimX = 150;
+		dimY = 16;
 		dispNF = new DecimalFormat("0.00");
 
-		vScaleBarPD = new vtkPolyData();
-		vtkPoints points = new vtkPoints();
-		vtkCellArray polys = new vtkCellArray();
-		vScaleBarPD.SetPoints(points);
-		vScaleBarPD.SetPolys(polys);
-
-		points.SetNumberOfPoints(4);
-
-		vtkIdList idList = new vtkIdList();
-		idList.SetNumberOfIds(4);
-		for (int i = 0; i < 4; ++i)
-			idList.SetId(i, i);
-		polys.InsertNextCell(idList);
-
-		vScaleBarM = new vtkPolyDataMapper2D();
-		vScaleBarM.SetInputData(vScaleBarPD);
-
-		vScaleBarA = new vtkActor2D();
-		vScaleBarA.SetMapper(vScaleBarM);
+		vScaleBarRP = new VtkRectPainter();
 
 		vScaleBarTA = new vtkTextActor();
-
-		vScaleBarA.GetProperty().SetColor(1.0, 1.0, 1.0);
-		vScaleBarA.GetProperty().SetOpacity(0.5);
 		vScaleBarTA.GetTextProperty().SetColor(0.0, 0.0, 0.0);
 		vScaleBarTA.GetTextProperty().SetJustificationToCentered();
 		vScaleBarTA.GetTextProperty().BoldOn();
@@ -112,7 +83,7 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 	 */
 	public double getSizeX()
 	{
-		return dimSizeX;
+		return dimX;
 	}
 
 	/**
@@ -120,7 +91,7 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 	 */
 	public double getSizeY()
 	{
-		return dimSizeY;
+		return dimY;
 	}
 
 	/**
@@ -154,13 +125,13 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 	{
 		// Bail if no state change
 		boolean isChanged = false;
-		isChanged |= Double.compare(dimSizeX, aSizeX) != 0;
-		isChanged |= Double.compare(dimSizeY, aSizeY) != 0;
+		isChanged |= Double.compare(dimX, aSizeX) != 0;
+		isChanged |= Double.compare(dimY, aSizeY) != 0;
 		if (isChanged == false)
 			return;
 
-		dimSizeX = aSizeX;
-		dimSizeY = aSizeY;
+		dimX = aSizeX;
+		dimY = aSizeY;
 
 		vtkUpdateState();
 	}
@@ -179,7 +150,7 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 
 		// Return the list of all vtkProps
 		List<vtkProp> retL = new ArrayList<>();
-		retL.add(vScaleBarA);
+		retL.addAll(vScaleBarRP.getProps());
 		retL.add(vScaleBarTA);
 		return retL;
 	}
@@ -193,7 +164,7 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 	@Override
 	public void vtkDispose()
 	{
-		vScaleBarA.Delete();
+		vScaleBarRP.vtkDispose();
 		vScaleBarTA.Delete();
 	}
 
@@ -202,9 +173,6 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 	{
 		updatePosition();
 		updateInfoMsg();
-
-		vScaleBarA.Modified();
-		vScaleBarTA.Modified();
 
 		// Send out the update notification
 		refRenderer.notifySceneChange();
@@ -221,11 +189,12 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 			return;
 
 		// Update the scale bar's text
-		double fullSpanKm = nominalPixelSpan * dimSizeX;
+		double fullSpanKm = nominalPixelSpan * dimX;
 		String tmpMsg = dispNF.format(fullSpanKm) + " km";
 		if (fullSpanKm < 1.0)
 			tmpMsg = dispNF.format(fullSpanKm * 1000.0) + " m";
 		vScaleBarTA.SetInput(tmpMsg);
+		vScaleBarTA.Modified();
 	}
 
 	/**
@@ -238,20 +207,17 @@ public class ScaleBarPainter implements ViewActionListener, VtkPropProvider, Vtk
 		vtkJoglPanelComponent tmpRenWin = refRenderer.getRenderWindowPanel();
 		int compW = tmpRenWin.getComponent().getWidth();
 		int padAmt = 7;
-		double x = compW - dimSizeX - padAmt;
+		double x = compW - dimX - padAmt;
 		double y = padAmt;
 
 		// Update scale bar
-		vtkPoints points = vScaleBarPD.GetPoints();
-		points.SetPoint(0, x, y, 0.0);
-		points.SetPoint(1, x + dimSizeX, y, 0.0);
-		points.SetPoint(2, x + dimSizeX, y + dimSizeY, 0.0);
-		points.SetPoint(3, x, y + dimSizeY, 0.0);
+		vScaleBarRP.setLocation(new Location(x, y, dimX, dimY));
 
 		// Update scale bar text
-		int fontSize = (int) (dimSizeY - 4);
-		vScaleBarTA.SetPosition(x + dimSizeX / 2, y + 2);
+		int fontSize = (int) (dimY - 4);
+		vScaleBarTA.SetPosition(x + dimX / 2, y + 2);
 		vScaleBarTA.GetTextProperty().SetFontSize(fontSize);
+		vScaleBarTA.Modified();
 	}
 
 }
