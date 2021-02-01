0a973dbb8dc4dd066f5e0df23bbc260a7e6b7aac
James Peachey <James.Peachey@jhuapl.edu>
2021-02-01 15:52:14 -0500

    RM 2238: update documentation, clarify that this is under construction.

--- src/edu/jhuapl/saavtk/util/file/VtkFileReader.java
+++ src/edu/jhuapl/saavtk/util/file/VtkFileReader.java
@@ -2,15 +2,20 @@ package edu.jhuapl.saavtk.util.file;
 
 import java.io.File;
 import java.io.IOException;
-import com.google.common.collect.ImmutableList;
 
 import edu.jhuapl.saavtk.util.file.DataFileInfo.FileFormat;
-import vtk.vtkPolyDataReader;
 
+/**
+ * This class is really still under construction. It doesn't work yet to load
+ * a plate coloring from a VTK file. Problems are described inline below.
+ * 
+ * @author James Peachey
+ *
+ */
 public class VtkFileReader extends DataFileReader
 {
     private static final VtkFileReader INSTANCE = new VtkFileReader();
-    
+
     public static VtkFileReader of()
     {
         return INSTANCE;
@@ -24,48 +29,61 @@ public class VtkFileReader extends DataFileReader
         }
     };
 
+    /**
+     * According to online searches, there is no good way to tell if a VTK file is
+     * valid in code without generating a bunch of error messages. Instead, do a
+     * simplistic file name check, allowing for optional gzipped format.
+     * 
+     * @param file the file to check
+     */
     @Override
     public void checkFormat(File file) throws IOException, FileFormatException
     {
-        if (isFileGzipped(file))
+        String lcFileName = file.getName().toLowerCase();
+        if (!lcFileName.endsWith(".vtk") && !lcFileName.endsWith(".vtk.gz"))
         {
-            file = gunzip(file);
-        }
-
-        vtkPolyDataReader fileReader = new vtkPolyDataReader();
-        fileReader.SetFileName(file.toString());
-        fileReader.Update();
-
-        if (0 == fileReader.OpenVTKFile()) {
             throw new IncorrectFileFormatException("File format check: unable to open as a VTK file " + file.toString());
         }
     }
 
+    /**
+     * There seems to be no good way to implement this such that it would work with
+     * the DataFileReader design.
+     */
     @Override
     public DataFileInfo readFileInfo(File file) throws IOException, FileFormatException
     {
-        if (isFileGzipped(file))
-        {
-            file = gunzip(file);
-        }
-
-        vtkPolyDataReader fileReader = new vtkPolyDataReader();
-        fileReader.SetFileName(file.toString());
-        fileReader.Update();
-
-        if (0 == fileReader.OpenVTKFile()) {
-            throw new IncorrectFileFormatException("Unable to open as a VTK file " + file.toString());
-        }
-
+//        if (isFileGzipped(file))
+//        {
+//            file = gunzip(file);
+//        }
+//
+//        vtkPolyDataReader fileReader = new vtkPolyDataReader();
+//        fileReader.SetFileName(file.toString());
+//
+//        // This doesn't work: IsFileValid seems to return the same value whether or not
+//        // the file is a VTK polydata file.
+//        if (0 == fileReader.IsFileValid("POLYDATA"))
+//        {
+//            throw new IncorrectFileFormatException("Unable to open as a VTK file " + file.toString());
+//        }
+//
+//        // This also doesn't work: OpenFile seems to return non-0 (meaning no error
+//        // according to VTK docs)
+//        // whether or not the file is a VTK polydata file.
+//        if (0 == fileReader.OpenVTKFile())
+//        {
+//            throw new IncorrectFileFormatException("Unable to open as a VTK file " + file.toString());
+//        }
 //        int numberElements;
 //        int numberComponents;
 //        fileReader.ReadArray("float", numberElements, numberComponents);
 //        vtkPolyData polyData = fileReader.GetOutput();
 //        
 //        polyData.data
-
-        ImmutableList.Builder<DataObjectInfo> builder = ImmutableList.builder();
-
+//
+//        ImmutableList.Builder<DataObjectInfo> builder = ImmutableList.builder();
+//
 //        List<String> fields;
 //        List<? extends InfoRow> elements;
 //        Description description = Description.of(fields, elements);
@@ -75,7 +93,8 @@ public class VtkFileReader extends DataFileReader
 //        DataObjectInfo info = TableInfo.of(file.getName(), description, numberRows, columnInfo);
 //        
 //        builder.add(info);
-        return DataFileInfo.of(file, VTK_FORMAT, builder.build());
+//        return DataFileInfo.of(file, VTK_FORMAT, builder.build());
+        throw new UnsupportedOperationException("VTK file reading is not yet implemented");
     }
 
 }
