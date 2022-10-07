512ad197676660ddd984ff0da452eedd82d4b9e9
Josh Steele <josh.steele@jhuapl.edu>
2022-10-07 13:02:00 -0400

    RM 1215: import updates

--- src/edu/jhuapl/saavtk/gui/panel/PolyhedralModelControlPanel.java
+++ src/edu/jhuapl/saavtk/gui/panel/PolyhedralModelControlPanel.java
@@ -1,8 +1,7 @@
 package edu.jhuapl.saavtk.gui.panel;
 
 import java.awt.BorderLayout;
-import java.awt.Cursor; 
-import java.awt.EventQueue;
+import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
@@ -35,9 +34,9 @@ import javax.swing.event.ChangeListener;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 
-import com.github.davidmoten.guavamini.Lists;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 
 import edu.jhuapl.saavtk.coloring.gui.ColoringModePanel;
 import edu.jhuapl.saavtk.gui.dialog.CustomFileChooser;
@@ -50,7 +49,6 @@ import edu.jhuapl.saavtk.model.PolyhedralModel;
 import edu.jhuapl.saavtk.model.plateColoring.BasicColoringDataManager;
 import edu.jhuapl.saavtk.model.plateColoring.ColoringData;
 import edu.jhuapl.saavtk.model.plateColoring.ColoringDataManager;
-import edu.jhuapl.saavtk.model.plateColoring.CustomizableColoringDataManager;
 import edu.jhuapl.saavtk.model.plateColoring.LoadableColoringData;
 import edu.jhuapl.saavtk.util.BoundingBox;
 import edu.jhuapl.saavtk.util.Configuration;
