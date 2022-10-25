aa667bf14c132a4629923c624401ef8e27b1cfad
James Peachey <jpeachey@users.noreply.github.com>
2022-10-25 14:07:41 -0400

    RM 2472: base class for new metadata factory approach.

---
+++ src/edu/jhuapl/saavtk/util/FactoryWithMetadataSupport.java
@@ -0,0 +1,73 @@
+package edu.jhuapl.saavtk.util;
+
+import crucible.crust.metadata.api.Key;
+import crucible.crust.metadata.api.Metadata;
+import crucible.crust.metadata.impl.InstanceGetter;
+import crucible.crust.metadata.impl.SettableMetadata;
+
+public abstract class FactoryWithMetadataSupport<T>
+{
+
+    protected FactoryWithMetadataSupport()
+    {
+        super();
+    }
+
+    public T of(Metadata md)
+    {
+        initializeSerializationProxy();
+        return instanceGetter().providesGenericObjectFromMetadata(getTargetKey()).provide(md);
+    }
+
+    public Metadata toMetadata(T t)
+    {
+        initializeSerializationProxy();
+        return instanceGetter().providesMetadataFromGenericObject(getTargetType()).provide(t);
+    }
+
+    public final void initializeSerializationProxy()
+    {
+        Key<T> mdKey = getTargetKey();
+
+        InstanceGetter instanceGetter = instanceGetter();
+
+        synchronized (getTargetType())
+        {
+            if (!instanceGetter.isProvidableFromMetadata(mdKey))
+            {
+                doInitProxy();
+            }
+        }
+    }
+
+    protected abstract Key<T> getTargetKey();
+
+    /**
+     * Returned type is used to lock during initialization, so this should always
+     * return the same exact {@link Class} instance.
+     * 
+     * @return
+     */
+    protected abstract Class<?> getTargetType();
+
+    protected abstract void doInitProxy();
+
+    protected InstanceGetter instanceGetter()
+    {
+        return InstanceGetter.defaultInstanceGetter();
+    }
+
+    protected <T1> T1 get(Key<T1> key, Metadata md, T1 defaultValue) {
+        T1 t = defaultValue;
+        if (md.hasKey(key)) {
+         t = md.get(key);   
+        }
+        
+        return t;
+    }
+
+    protected <T1> void set(Key<T1> key, T1 value, SettableMetadata md) {
+        md.put(key, value);
+    }
+
+}
