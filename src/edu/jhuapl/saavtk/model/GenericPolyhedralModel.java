aa3912d5a3b4bc128a594afb5d923a54e71d27ef
Josh Steele <josh.steele@jhuapl.edu>
2023-01-06 15:44:41 -0500

    RM 1215-2356: more debugging

--- src/edu/jhuapl/saavtk/model/GenericPolyhedralModel.java
+++ src/edu/jhuapl/saavtk/model/GenericPolyhedralModel.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -1750,6 +1751,9 @@ public class GenericPolyhedralModel extends PolyhedralModel
             return modelNames[resolutionLevel];
         else
         {
+        	System.out.println("GenericPolyhedralModel: getModelName: resolution level " + resolutionLevel);
+        	System.out.println("GenericPolyhedralModel: getModelName: number of model names " + modelNames.length);
+        	System.out.println("GenericPolyhedralModel: getModelName: model names " + Arrays.toString(modelNames));
         	System.out.println("GenericPolyhedralModel: getModelName: no model name available");
             return null;
         }
