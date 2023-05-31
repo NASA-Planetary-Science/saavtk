e2a40419fbe116355e6bad1af51ed365e10bc066
carrbd1 <carrbd1@CARRBD1-WL1.dom1.jhuapl.edu>
2023-05-31 14:47:14 -0400

    RM 2268: Key shortcut changes radio selection for surface/wireframe

--- src/edu/jhuapl/saavtk/main/gui/ShapeModelEditPanel.java
+++ src/edu/jhuapl/saavtk/main/gui/ShapeModelEditPanel.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.text.DecimalFormat;
 
 import javax.swing.JButton;
@@ -37,7 +39,7 @@ import net.miginfocom.swing.MigLayout;
  *
  * @author lopeznr1
  */
-public class ShapeModelEditPanel extends JPanel implements ActionListener, ItemListener, GridChangeListener
+public class ShapeModelEditPanel extends JPanel implements ActionListener, ItemListener, KeyListener, GridChangeListener
 {
 	// Constants
 	private static final Range<Double> OpacityRange = Range.closed(0.0, 1.0);
@@ -133,6 +135,7 @@ public class ShapeModelEditPanel extends JPanel implements ActionListener, ItemL
 		// Representation area
 		representationL = new JLabel("Representation:");
 		representationBox = new GComboBox<>(this, Representation.values());
+		representationBox.addKeyListener(this);
 		representationPanel = new CardPanel<>();
 		representationPanel.addCard(Representation.Points, pointSizePanel);
 		representationPanel.addCard(Representation.Wireframe, lineWidthPanel);
@@ -399,6 +402,35 @@ public class ShapeModelEditPanel extends JPanel implements ActionListener, ItemL
 		isEnabled = isShown == true;
 		isEnabled &= pointSizeNFS.getValue() != 1.0;
 		pointSizeResetB.setEnabled(isEnabled);
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent aEvent)
+	{
+		int keyCode = aEvent.getKeyCode();
+		if (keyCode == KeyEvent.VK_S)
+		{
+			representationBox.setChosenItem(Representation.Surface);
+		}
+		else if (keyCode == KeyEvent.VK_W)
+		{
+			representationBox.setChosenItem(Representation.Wireframe);
+		}
+		else
+		{
+			return;
+		}
+		updateGui();
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 	}
 
 }
