16e2a5c39f6b65acd871ffb84881863a5d4659e7
James Peachey <jpeachey@users.noreply.github.com>
2023-06-14 08:59:51 -0400

    RM 2554: auto-format prior to refactoring (no code changes).

--- src/edu/jhuapl/saavtk/model/structure/Polygon.java
+++ src/edu/jhuapl/saavtk/model/structure/Polygon.java
@@ -17,61 +17,60 @@ import edu.jhuapl.saavtk.util.LatLon;
 
 public class Polygon
 {
-	private static Configurable formConfigurationFor(edu.jhuapl.saavtk.structure.Polygon aPolygon)
-	{
-		Configurable lineConfiguration = Line.formConfigurationFor(aPolygon);
+    private static Configurable formConfigurationFor(edu.jhuapl.saavtk.structure.Polygon aPolygon)
+    {
+        Configurable lineConfiguration = Line.formConfigurationFor(aPolygon);
 
-		KeyedFactory.Builder<Object> builder = KeyedFactory.instance().builder();
-		for (ControlKey<?> key : lineConfiguration.getKeys())
-		{
-			@SuppressWarnings("unchecked")
-			ControlKey<Viewable> contentKey = (ControlKey<Viewable>) key;
-			builder.put(contentKey, lineConfiguration.getItem(contentKey));
-		}
-		builder.put(AREA_KEY, SettableStoredFactory.instance().of(aPolygon.getSurfaceArea()));
-		builder.put(SHOW_INTERIOR_KEY, SettableStoredFactory.instance().of(aPolygon.getShowInterior()));
+        KeyedFactory.Builder<Object> builder = KeyedFactory.instance().builder();
+        for (ControlKey<?> key : lineConfiguration.getKeys())
+        {
+            @SuppressWarnings("unchecked")
+            ControlKey<Viewable> contentKey = (ControlKey<Viewable>) key;
+            builder.put(contentKey, lineConfiguration.getItem(contentKey));
+        }
+        builder.put(AREA_KEY, SettableStoredFactory.instance().of(aPolygon.getSurfaceArea()));
+        builder.put(SHOW_INTERIOR_KEY, SettableStoredFactory.instance().of(aPolygon.getShowInterior()));
 
-		return ConfigurableFactory.instance().of(lineConfiguration.getVersion(), builder.build());
-	}
+        return ConfigurableFactory.instance().of(lineConfiguration.getVersion(), builder.build());
+    }
 
-	private static final Key<edu.jhuapl.saavtk.structure.Polygon> POLYGON_STRUCTURE_PROXY_KEY = Key.of("Polygon");
-	private static boolean proxyInitialized = false;
+    private static final Key<edu.jhuapl.saavtk.structure.Polygon> POLYGON_STRUCTURE_PROXY_KEY = Key.of("Polygon");
+    private static boolean proxyInitialized = false;
 
-	public static void initializeSerializationProxy()
-	{
-		if (!proxyInitialized)
-		{
-			LatLon.initializeSerializationProxy();
+    public static void initializeSerializationProxy()
+    {
+        if (!proxyInitialized)
+        {
+            LatLon.initializeSerializationProxy();
 
-			InstanceGetter.defaultInstanceGetter().register(POLYGON_STRUCTURE_PROXY_KEY, source -> {
-				int id = source.get(Key.of(Line.ID.getId()));
-				edu.jhuapl.saavtk.structure.Polygon result = new edu.jhuapl.saavtk.structure.Polygon(id, null, new ArrayList<>());
-				unpackMetadata(source, result);
+            InstanceGetter.defaultInstanceGetter().register(POLYGON_STRUCTURE_PROXY_KEY, source -> {
+                int id = source.get(Key.of(Line.ID.getId()));
+                edu.jhuapl.saavtk.structure.Polygon result = new edu.jhuapl.saavtk.structure.Polygon(id, null, new ArrayList<>());
+                unpackMetadata(source, result);
 
-				return result;
-			}, edu.jhuapl.saavtk.structure.Polygon.class, polygon -> {
-				Configurable configuration = formConfigurationFor(polygon);
-				return KeyValueCollectionMetadataManager.of(configuration.getVersion(), configuration).store();
-			});
+                return result;
+            }, edu.jhuapl.saavtk.structure.Polygon.class, polygon -> {
+                Configurable configuration = formConfigurationFor(polygon);
+                return KeyValueCollectionMetadataManager.of(configuration.getVersion(), configuration).store();
+            });
 
-			proxyInitialized = true;
-		}
-	}
+            proxyInitialized = true;
+        }
+    }
 
-	public static final ControlKey<SettableStored<Double>> AREA_KEY = SettableStoredFactory.key("area");
-	public static final ControlKey<SettableStored<Boolean>> SHOW_INTERIOR_KEY = SettableStoredFactory
-			.key("showInterior");
+    public static final ControlKey<SettableStored<Double>> AREA_KEY = SettableStoredFactory.key("area");
+    public static final ControlKey<SettableStored<Boolean>> SHOW_INTERIOR_KEY = SettableStoredFactory.key("showInterior");
 
-	protected static void unpackMetadata(Metadata source, edu.jhuapl.saavtk.structure.Polygon polygon)
-	{
-		Line.unpackMetadata(source, polygon);
+    protected static void unpackMetadata(Metadata source, edu.jhuapl.saavtk.structure.Polygon polygon)
+    {
+        Line.unpackMetadata(source, polygon);
 
-		Key<Double> areaKey = Key.of(AREA_KEY.getId());
-		if (source.hasKey(areaKey))
-			polygon.setSurfaceArea(source.get(areaKey));
+        Key<Double> areaKey = Key.of(AREA_KEY.getId());
+        if (source.hasKey(areaKey))
+            polygon.setSurfaceArea(source.get(areaKey));
 
-		Key<Boolean> showInteriorKey = Key.of(SHOW_INTERIOR_KEY.getId());
-		if (source.hasKey(showInteriorKey))
-			polygon.setShowInterior(source.get(showInteriorKey));
-	}
+        Key<Boolean> showInteriorKey = Key.of(SHOW_INTERIOR_KEY.getId());
+        if (source.hasKey(showInteriorKey))
+            polygon.setShowInterior(source.get(showInteriorKey));
+    }
 }
