684c696fe4ace4e5d78096dca3a40ee60fa17417
James Peachey <jpeachey@users.noreply.github.com>
2023-06-14 08:48:15 -0400

    RM 2554: auto-format prior to refactoring (no code change).

--- src/edu/jhuapl/saavtk/model/structure/Line.java
+++ src/edu/jhuapl/saavtk/model/structure/Line.java
@@ -27,90 +27,90 @@ import edu.jhuapl.saavtk.vtk.font.FontAttr;
 
 public class Line
 {
-	public static final ControlKey<SettableStored<Integer>> ID = SettableStoredFactory.key("id");
-	public static final ControlKey<SettableStored<String>> NAME = SettableStoredFactory.key("name");
-	public static final ControlKey<Stored<List<LatLon>>> VERTICES = StoredFactory.key("vertices");
-	public static final ControlKey<SettableStored<int[]>> COLOR = SettableStoredFactory.key("color");
-	public static final ControlKey<SettableStored<String>> LABEL = SettableStoredFactory.key("label");
-	public static final ControlKey<SettableStored<int[]>> LABEL_COLOR = SettableStoredFactory.key("labelColor");
-	public static final ControlKey<SettableStored<Integer>> LABEL_FONT_SIZE = SettableStoredFactory.key("labelFontSize");
-	public static final ControlKey<SettableStored<Boolean>> HIDDEN = SettableStoredFactory.key("hidden");
-	public static final ControlKey<SettableStored<Boolean>> LABEL_HIDDEN = SettableStoredFactory.key("labelHidden");
+    public static final ControlKey<SettableStored<Integer>> ID = SettableStoredFactory.key("id");
+    public static final ControlKey<SettableStored<String>> NAME = SettableStoredFactory.key("name");
+    public static final ControlKey<Stored<List<LatLon>>> VERTICES = StoredFactory.key("vertices");
+    public static final ControlKey<SettableStored<int[]>> COLOR = SettableStoredFactory.key("color");
+    public static final ControlKey<SettableStored<String>> LABEL = SettableStoredFactory.key("label");
+    public static final ControlKey<SettableStored<int[]>> LABEL_COLOR = SettableStoredFactory.key("labelColor");
+    public static final ControlKey<SettableStored<Integer>> LABEL_FONT_SIZE = SettableStoredFactory.key("labelFontSize");
+    public static final ControlKey<SettableStored<Boolean>> HIDDEN = SettableStoredFactory.key("hidden");
+    public static final ControlKey<SettableStored<Boolean>> LABEL_HIDDEN = SettableStoredFactory.key("labelHidden");
 
-	private static final Versionable CONFIGURATION_VERSION = Version.of(1, 0);
-	private static final SettableStoredFactory settableValues = SettableStoredFactory.instance();
+    private static final Versionable CONFIGURATION_VERSION = Version.of(1, 0);
+    private static final SettableStoredFactory settableValues = SettableStoredFactory.instance();
 
-	protected static Configurable formConfigurationFor(PolyLine aLine)
-	{
-		int[] intArr;
+    protected static Configurable formConfigurationFor(PolyLine aLine)
+    {
+        int[] intArr;
 
-		KeyedFactory.Builder<Object> builder = KeyedFactory.instance().builder();
+        KeyedFactory.Builder<Object> builder = KeyedFactory.instance().builder();
 
-		builder.put(ID, settableValues.of(aLine.getId()));
-		builder.put(NAME, settableValues.of(aLine.getName()));
-		// Note: it is correct to use settableValues to instantiate the setting for
-		// VERTICES. This is because the list of controlPoints is final but mutable. If
-		// one used just "Values", the set of vertices would not be saved in the file
-		// because it would not be considered "stateful".
-		builder.put(VERTICES, settableValues.of(aLine.getControlPoints()));
-		intArr = StructureLegacyUtil.convertColorToRgba(aLine.getColor());
-		builder.put(COLOR, settableValues.of(intArr));
-		builder.put(LABEL, settableValues.of(aLine.getLabel()));
+        builder.put(ID, settableValues.of(aLine.getId()));
+        builder.put(NAME, settableValues.of(aLine.getName()));
+        // Note: it is correct to use settableValues to instantiate the setting for
+        // VERTICES. This is because the list of controlPoints is final but mutable. If
+        // one used just "Values", the set of vertices would not be saved in the file
+        // because it would not be considered "stateful".
+        builder.put(VERTICES, settableValues.of(aLine.getControlPoints()));
+        intArr = StructureLegacyUtil.convertColorToRgba(aLine.getColor());
+        builder.put(COLOR, settableValues.of(intArr));
+        builder.put(LABEL, settableValues.of(aLine.getLabel()));
 
-		FontAttr tmpFA = aLine.getLabelFontAttr();
-		intArr = StructureLegacyUtil.convertColorToRgba(tmpFA.getColor());
-		builder.put(LABEL_COLOR, settableValues.of(intArr));
-		builder.put(LABEL_FONT_SIZE, settableValues.of(tmpFA.getSize()));
-		builder.put(HIDDEN, settableValues.of(!aLine.getVisible()));
-		builder.put(LABEL_HIDDEN, settableValues.of(!tmpFA.getIsVisible()));
+        FontAttr tmpFA = aLine.getLabelFontAttr();
+        intArr = StructureLegacyUtil.convertColorToRgba(tmpFA.getColor());
+        builder.put(LABEL_COLOR, settableValues.of(intArr));
+        builder.put(LABEL_FONT_SIZE, settableValues.of(tmpFA.getSize()));
+        builder.put(HIDDEN, settableValues.of(!aLine.getVisible()));
+        builder.put(LABEL_HIDDEN, settableValues.of(!tmpFA.getIsVisible()));
 
-		return ConfigurableFactory.instance().of(CONFIGURATION_VERSION, builder.build());
-	}
+        return ConfigurableFactory.instance().of(CONFIGURATION_VERSION, builder.build());
+    }
 
-	private static final Key<PolyLine> LINE_STRUCTURE_PROXY_KEY = Key.of("Line (structure)");
-	private static boolean proxyInitialized = false;
+    private static final Key<PolyLine> LINE_STRUCTURE_PROXY_KEY = Key.of("Line (structure)");
+    private static boolean proxyInitialized = false;
 
-	public static void initializeSerializationProxy()
-	{
-		if (!proxyInitialized)
-		{
-			LatLon.initializeSerializationProxy();
+    public static void initializeSerializationProxy()
+    {
+        if (!proxyInitialized)
+        {
+            LatLon.initializeSerializationProxy();
 
-			InstanceGetter.defaultInstanceGetter().register(LINE_STRUCTURE_PROXY_KEY, source -> {
-				int id = source.get(Key.of(ID.getId()));
-				PolyLine result = new PolyLine(id, null, new ArrayList<>());
-				unpackMetadata(source, result);
+            InstanceGetter.defaultInstanceGetter().register(LINE_STRUCTURE_PROXY_KEY, source -> {
+                int id = source.get(Key.of(ID.getId()));
+                PolyLine result = new PolyLine(id, null, new ArrayList<>());
+                unpackMetadata(source, result);
 
-				return result;
-			}, PolyLine.class, line -> {
-				Configurable configuration = formConfigurationFor(line);
-				return KeyValueCollectionMetadataManager.of(configuration.getVersion(), configuration).store();
-			});
+                return result;
+            }, PolyLine.class, line -> {
+                Configurable configuration = formConfigurationFor(line);
+                return KeyValueCollectionMetadataManager.of(configuration.getVersion(), configuration).store();
+            });
 
-			proxyInitialized = true;
-		}
-	}
+            proxyInitialized = true;
+        }
+    }
 
-	protected static void unpackMetadata(Metadata source, PolyLine line)
-	{
-		int[] intArr;
+    protected static void unpackMetadata(Metadata source, PolyLine line)
+    {
+        int[] intArr;
 
-		line.setName(source.get(Key.of(NAME.getId())));
-		intArr = source.get(Key.of(COLOR.getId()));
-		line.setColor(StructureLegacyUtil.convertRgbaToColor(intArr));
+        line.setName(source.get(Key.of(NAME.getId())));
+        intArr = source.get(Key.of(COLOR.getId()));
+        line.setColor(StructureLegacyUtil.convertRgbaToColor(intArr));
 
-		List<LatLon> sourceControlPoints = source.get(Key.of(VERTICES.getId()));
-		line.setControlPoints(ImmutableList.copyOf(sourceControlPoints));
+        List<LatLon> sourceControlPoints = source.get(Key.of(VERTICES.getId()));
+        line.setControlPoints(ImmutableList.copyOf(sourceControlPoints));
 
-		line.setLabel(source.get(Key.of(LABEL.getId())));
-		intArr = source.get(Key.of(LABEL_COLOR.getId()));
-		Color labelColor = StructureLegacyUtil.convertRgbaToColor(intArr);
-		int labelSize = source.get(Key.of(LABEL_FONT_SIZE.getId()));
-		boolean visible = source.get(Key.of(HIDDEN.getId()));
-		line.setVisible(!visible);
-		boolean labelIsVisible = source.get(Key.of(LABEL_HIDDEN.getId()));
-		labelIsVisible = !labelIsVisible;
+        line.setLabel(source.get(Key.of(LABEL.getId())));
+        intArr = source.get(Key.of(LABEL_COLOR.getId()));
+        Color labelColor = StructureLegacyUtil.convertRgbaToColor(intArr);
+        int labelSize = source.get(Key.of(LABEL_FONT_SIZE.getId()));
+        boolean visible = source.get(Key.of(HIDDEN.getId()));
+        line.setVisible(!visible);
+        boolean labelIsVisible = source.get(Key.of(LABEL_HIDDEN.getId()));
+        labelIsVisible = !labelIsVisible;
 
-		line.setLabelFontAttr(new FontAttr("Plain", labelColor, labelSize, labelIsVisible));
-	}
+        line.setLabelFontAttr(new FontAttr("Plain", labelColor, labelSize, labelIsVisible));
+    }
 }
