15128b2227570b2a441d3e7a19bd28bdc6f03986
Josh Steele <josh.steele@jhuapl.edu>
2023-08-17 09:28:54 -0400

    Corrected a line that shouldn't have been commented out.

--- src/edu/jhuapl/saavtk/model/GenericPolyhedralModel.java
+++ src/edu/jhuapl/saavtk/model/GenericPolyhedralModel.java
@@ -1744,6 +1744,7 @@ public class GenericPolyhedralModel extends PolyhedralModel
             resolutionLevel = getNumberResolutionLevels() - 1;
 
         reloadShapeModel();
+        this.pcs.firePropertyChange(Properties.MODEL_RESOLUTION_CHANGED, null, null);
         
     }
 
