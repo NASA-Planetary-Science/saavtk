d284c0326bacb569078cdf1a9494773d1562bca9
Josh Steele <josh.steele@jhuapl.edu>
2024-02-01 14:22:36 -0500

    Cleaned up imports

--- src/edu/jhuapl/saavtk/gui/render/Renderer.java
+++ src/edu/jhuapl/saavtk/gui/render/Renderer.java
@@ -26,7 +26,6 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
