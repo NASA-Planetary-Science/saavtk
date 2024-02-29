cafc26bf8f37524aa853ce1032048accfee615ea
James Peachey <James.Peachey@jhuapl.edu>
2024-02-29 12:55:02 -0500

    Hot-fix: include the URL in messages.

--- src/edu/jhuapl/saavtk/util/UnauthorizedAccessException.java
+++ src/edu/jhuapl/saavtk/util/UnauthorizedAccessException.java
@@ -3,25 +3,27 @@ package edu.jhuapl.saavtk.util;
 import java.net.URL;
 
 // TODO this should extend Exception and thus be checked.
-public class UnauthorizedAccessException extends RuntimeException
-{
-    private static final long serialVersionUID = 7671006960310656926L;
-    private final URL url;
+public class UnauthorizedAccessException extends RuntimeException {
+	private static final long serialVersionUID = 7671006960310656926L;
+	private final URL url;
 
-    public UnauthorizedAccessException(String cause, URL url)
-    {
-        super(cause);
-        this.url = url;
-    }
+	public UnauthorizedAccessException(String cause, URL url) {
+		super(cause + url);
+		this.url = url;
+	}
 
-    public UnauthorizedAccessException(Exception cause, URL url)
-    {
-        super(cause);
-        this.url = url;
-    }
+	public UnauthorizedAccessException(Exception cause, URL url) {
+		super(url != null ? url.toString() : "null", cause);
+		this.url = url;
+	}
 
-    public URL getURL()
-    {
-        return url;
-    }
+	public URL getURL() {
+		return url;
+	}
+
+	@Override
+	public String toString() {
+		return getMessage();
+	}
+
 }
